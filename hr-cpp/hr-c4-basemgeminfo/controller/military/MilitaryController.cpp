90204b3285161b27fef586a058ec089aa4654216
BitCicada <1203729911@qq.com>
2023-05-15 22:03:35 +0800

    修改指定员工军转干部(单条修改)

--- hr-cpp/hr-c4-basemgeminfo/controller/military/MilitaryController.cpp
+++ hr-cpp/hr-c4-basemgeminfo/controller/military/MilitaryController.cpp
@@ -8,3 +8,12 @@ MilitaryJsonVO::Wrapper MilitaryController::execQueryMilitary(const MilitaryQuer
 	vo->success(MilitaryDTO::Wrapper::createShared());
 	return vo;
 }
+
+Uint64JsonVO::Wrapper MilitaryController::execModifyMilitary(const MilitaryDTO::Wrapper& dto)
+{
+	// 创建响应对象
+	auto vo = Uint64JsonVO::createShared();
+	// 响应结果
+	vo->success(1);
+	return vo;
+}
