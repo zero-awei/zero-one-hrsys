2d380b2e0acfd6cbc896d4f834d0f0759ef2bd46
Triple Gold <1805882195@qq.com>
2023-05-16 18:45:12 +0800

    定义领域模型，实现接口

--- hr-cpp/hr-c2-orgmanager/controller/LegalEntityMai/LegalEntityMaiController.h
+++ hr-cpp/hr-c2-orgmanager/controller/LegalEntityMai/LegalEntityMaiController.h
@@ -38,8 +38,10 @@ public: // 定义接口
 	ENDPOINT_INFO(queryLegalEntityMai) {
 		// 定义接口标题
 		info->summary = ZH_WORDS_GETTER("LegalEntityMai.query.summary");
+		// 定义默认授权参数（可选定义，如果定义了，下面ENDPOINT里面需要加入API_HANDLER_AUTH_PARAME）
+		API_DEF_ADD_AUTH();
 		// 定义响应参数格式
-		API_DEF_ADD_RSP_JSON(LegalEntityMaiPageJsonVO);
+		API_DEF_ADD_RSP_JSON_WRAPPER(LegalEntityMaiPageJsonVO);
 		// 定义分页参数描述
 		API_DEF_ADD_PAGE_PARAMS();
 		// 定义其他查询参数描述
