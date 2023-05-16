4ea1fc88ce34574056f07101d5d7c5732721f5f8
小小怪将军 <3022984471@qq.com>
2023-05-16 22:06:33 +0800

    修改

---
+++ hr-frontend/src/components/pagination/Pagination.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="block">
+    <span class="demonstration">直接前往</span>
+    <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      v-model:currentPage="currentPage3"
+      :page-size="100"
+      layout="prev, pager, next, jumper"
+      :total="1000"
+    >
+    </el-pagination>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'pagination',
+  methods: {
+    handleSizeChange(val) {
+      console.log(`每页 ${val} 条`)
+    },
+    handleCurrentChange(val) {
+      console.log(`当前页: ${val}`)
+    }
+  },
+  data() {
+    return {
+      currentPage1: 5,
+      currentPage2: 5,
+      currentPage3: 5,
+      currentPage4: 4
+    }
+  }
+}
+</script>
+
+<style scoped></style>
