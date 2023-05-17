0a1bd4b2475b7bb702545d0dceaba06b421198d4
wxs <1595849283@qq.com>
2023-05-17 12:12:51 +0800

    员工状态下拉列表的领域模型和接口

---
+++ hr-java/hr-j2-employeelist/src/main/java/com/zeroone/star/employeelist/EmployeeStatusListApplication.java
@@ -0,0 +1,23 @@
+package com.zeroone.star.employeelist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * <p>
+ * 描述：服务器启动入口
+ * </p>
+ * <p>版权：&copy;01星球</p>
+ * <p>地址：01星球总部</p>
+ *
+ * @author 浅墨
+ * @version 1.0.0
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class EmployeeStatusListApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeStatusListApplication.class, args);
+    }
+}
