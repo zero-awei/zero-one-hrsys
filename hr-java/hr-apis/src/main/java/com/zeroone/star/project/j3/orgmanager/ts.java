059aad5441e48a9c15c4efa41aec7dae3f7f0eaa
zh <1525213943@qq.com>
2023-05-18 21:56:08 +0800

    j3添加了apis和domain包下的小组包（ts.java占位用，否则git无法识别创建了包，删除即可）

---
+++ hr-java/hr-apis/src/main/java/com/zeroone/star/project/j3/orgmanager/ts.java
@@ -0,0 +1,15 @@
+package com.zeroone.star.project.j3.orgmanager;
+
+/**
+ * <p>
+ * 描述：TODO
+ * </p>
+ * <p>版权：&copy;01星球</p>
+ * <p>地址：01星球总部</p>
+ *
+ * @author H
+ * @version 1.0.0
+ * @Date 2023/5/18 21:46
+ */
+public class ts {
+}
