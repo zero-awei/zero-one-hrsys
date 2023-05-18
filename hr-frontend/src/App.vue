406efaa4f7dad3178299c48e3a10f8daf045102a
LZF <lzf847490420@163.com>
2023-05-18 12:21:29 +0800

    Update App.vue
    测试

--- hr-frontend/src/App.vue
+++ hr-frontend/src/App.vue
@@ -1,3 +1,16 @@
 <template>
-  <router-view />
+  <!-- <router-view /> -->
+  <TableHead :title="title" :arrs ="arrs"/>
 </template>
+
+<script setup>
+import TableHead from '@/components/table/head/table-head.vue'
+
+const title = ref('人员列表')
+const arrs = reactive([
+  {
+    is:true,
+    name:'新增'
+  }
+])
+</script>
