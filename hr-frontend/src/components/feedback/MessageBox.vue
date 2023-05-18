7d1332d36b2bd1392098af76add9625afb1c0577
smalsong <18717536651@163.com>
2023-05-18 10:13:02 +0800

    新增事件处理  修改样式

---
+++ hr-frontend/src/components/feedback/MessageBox.vue
@@ -0,0 +1,36 @@
+<template>
+  <el-button text @click="showMsg()">操作</el-button>
+  <button @click="changeContent">警告内容</button>
+</template>
+
+<script setup>
+let messageInfo = ref('确定删除吗？')
+let message = ref('保存成功')
+let messageError = ref('校验失败')
+
+const showMsg = () => {
+  ElMessageBox.confirm(messageInfo.value, '警告', {
+    confirmButtonText: '确定',
+    cancelButtonText: '取消',
+    type: 'warning'
+  })
+    .then(() => {
+      ElMessage({
+        title: '成功',
+        type: 'success',
+        message: message.value
+      })
+    })
+    .catch(() => {
+      ElMessage({
+        type: 'info',
+        message: messageError.value
+      })
+    })
+}
+const changeContent=()=>{
+  messageInfo.value='确定修改吗?'
+  message='某某操作成功'
+  messageError='值规则校验异常'
+}
+</script>
--- hr-frontend/src/components/feedback/Notification.vue
+++ hr-frontend/src/components/feedback/Notification.vue
@@ -1,33 +1,35 @@
 <template>
-  <el-button plain @click="open1">成功</el-button>
-  <el-button plain @click="open2">警告</el-button>
-  <el-button plain @click="open3">错误</el-button>
+  <el-button plain @click="showSuccess">成功</el-button>
+  <el-button plain @click="showWarn">警告</el-button>
+  <el-button plain @click="showError">错误</el-button>
 </template>
 
 <script setup>
-import { ElNotification } from 'element-plus'
-
-const open1 = () => {
+let message = ref('成功')
+const showSuccess = () => {
   ElNotification({
     title: '成功',
     message: 'success message',
-    type: 'success'
+    type: 'success',
+    position: 'top-right',
   })
 }
 
-const open2 = () => {
+const showWarn = () => {
   ElNotification({
     title: '警告',
     message: 'warning message',
-    type: 'warning'
+    type: 'warning',
+    position: 'top-right',
   })
 }
 
-const open3 = () => {
+const showError = () => {
   ElNotification({
     title: '错误',
     message: 'error message',
-    type: 'error'
+    type: 'error',
+    position: 'top-right',
   })
 }
 </script>
