ea1fddf28725263d472e189322c93a5cef5bbc34
Vainglory-Vinda <1838598291@qq.com>
2023-05-18 22:25:43 +0800

    Squashed commit of the following:
    commit 4d9ab4e7d06955184f4845c96c568ae10c1f6319
    Merge: 4de17c9 059aad5
    Author: Vainglory-Vinda <1838598291@qq.com>
    Date:   Thu May 18 21:57:48 2023 +0800
    
        Merge branch 'j3' of github.com:zero-awei/zero-one-hrsys into j3
    
    commit 059aad5441e48a9c15c4efa41aec7dae3f7f0eaa
    Author: zh <1525213943@qq.com>
    Date:   Thu May 18 21:56:08 2023 +0800
    
        j3添加了apis和domain包下的小组包（ts.java占位用，否则git无法识别创建了包，删除即可）
    
    commit 9fe2edce0c62faf95a0c09f43d44bda29e85d946
    Author: zh <1525213943@qq.com>
    Date:   Thu May 18 21:41:19 2023 +0800
    
        修改端口号

---
+++ hr-java/hr-domain/src/main/java/com/zeroone/star/project/j3/vo/ts.java
@@ -0,0 +1,15 @@
+package com.zeroone.star.project.j3.vo;
+
+/**
+ * <p>
+ * 描述：TODO
+ * </p>
+ * <p>版权：&copy;01星球</p>
+ * <p>地址：01星球总部</p>
+ *
+ * @author H
+ * @version 1.0.0
+ * @Date 2023/5/18 21:52
+ */
+public class ts {
+}
