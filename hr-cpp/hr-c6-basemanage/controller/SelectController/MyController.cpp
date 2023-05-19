41e63599da3f4ae4b3dc67e4f58ca6356dfdd5a3
17853318082 <87115497+17853318082@users.noreply.github.com>
2023-05-19 19:26:43 +0800

    vs2019-2022

---
+++ hr-cpp/hr-c6-basemanage/controller/SelectController/MyController.cpp
@@ -0,0 +1,66 @@
+#include "stdafx.h"
+#include "MyController.h"
+
+StringJsonVO::Wrapper TestController::execQueryTest(const PageQuery::Wrapper & query)
+{
+	auto vo = StringJsonVO::createShared();
+	vo->success("test qurey success");
+	return vo;
+}
+/*
+StringJsonVO::Wrapper TestController::execQueryTest1(const PageQuery::Wrapper& query1)
+{
+	auto vo = StringJsonVO::createShared();
+	vo->success("test qurey1 success");
+	return vo;
+}
+*/
+
+/*
+Uint64JsonVO::Wrapper TestController::execAddSample(const SampleDTO::Wrapper& dto)
+{
+	// 定义返回数据对象
+	auto jvo = Uint64JsonVO::createShared();
+	// 参数校验
+	// 非空校验
+	if (!dto->age || !dto->name || !dto->sex)
+	{
+		jvo->init(UInt64(-1), RS_PARAMS_INVALID);
+		return jvo;
+	}
+	// 有效值校验
+	if (dto->age < 0 || dto->name->empty() || dto->sex->empty())
+	{
+		jvo->init(UInt64(-1), RS_PARAMS_INVALID);
+		return jvo;
+	}
+
+	//响应结果
+	return jvo;
+}
+*/
+
+Uint64JsonVO::Wrapper TestController::execAddSample1(const ContractDTO::Wrapper& dto)
+{
+	// 定义返回数据对象
+	auto jvo = Uint64JsonVO::createShared();
+	// 响应结果
+	return jvo;
+}
+
+Uint64JsonVO::Wrapper TestController::execModifySample(const ContractDTO::Wrapper& dto)
+{
+	// 定义返回数据对象
+	auto jvo = Uint64JsonVO::createShared();
+	// 响应结果
+	return jvo;
+}
+
+Uint64JsonVO::Wrapper TestController::execRemoveSample(const ContractDTO::Wrapper& dto)
+{
+	// 定义返回数据对象
+	auto jvo = Uint64JsonVO::createShared();
+
+	// 响应结果
+	return jvo;
+}
