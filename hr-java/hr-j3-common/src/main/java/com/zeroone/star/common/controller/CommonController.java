114a69f33eb7e09ec092339768391c875a45c6f0
loop <2784697456@qq.com>
2023-05-20 23:10:12 +0800

    合并语言能力接口到公共接口中

--- hr-java/hr-j3-common/src/main/java/com/zeroone/star/common/controller/CommonController.java
+++ hr-java/hr-j3-common/src/main/java/com/zeroone/star/common/controller/CommonController.java
@@ -1,8 +1,11 @@
 package com.zeroone.star.common.controller;
 
+import com.zeroone.star.project.dto.PageDTO;
 import com.zeroone.star.project.j3.common.CommonApis;
 import com.zeroone.star.project.j3.dto.DropdownListOptionDTO;
+import com.zeroone.star.project.j3.dto.languageability.LanguageAbilityDTO;
 import com.zeroone.star.project.j3.query.common.OneConditionQuery;
+import com.zeroone.star.project.j3.query.languageability.LanguageAbilityQuery;
 import com.zeroone.star.project.vo.JsonVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -77,6 +80,13 @@ public class CommonController implements CommonApis {
 
     @Override
     public JsonVO<Map<Integer, String>> SchoolNatureDropdownList() {
+        return null;
+    }
+
+    @GetMapping("queryList")
+    @ApiOperation("分页查询某人的语言能力")
+    @Override
+    public JsonVO<PageDTO<List<LanguageAbilityDTO>>> queryLanguageAbilityList(LanguageAbilityQuery personId) {
         return null;
     }
 }
