b523aee731907314856cdd2db62326b6f9f4e1a6
Triple Gold <1805882195@qq.com>
2023-05-20 18:04:47 +0800

    service层

---
+++ hr-cpp/hr-c2-orgmanager/service/LegalEntityMai/LegalEntityMaiService.cpp
@@ -0,0 +1,32 @@
+#include "stdafx.h"
+#include "LegalEntityMaiService.h"
+
+//LegalEntityMaiPageDTO::Wrapper LegalEntityMaiService::listAll(const LegalEntityMaiQuery::Wrapper& query)
+//{
+//	
+//}
+//
+//bool LegalEntityMaiService::updateData(const LegalEntityMaiDTO::Wrapper& dto)
+//{
+//
+//}
+//
+//bool LegalEntityMaiService::addData(const LegalEntityMaiDTO::Wrapper& dto)
+//{
+//
+//}
+//
+//bool LegalEntityMaiService::removeData(string id)
+//{
+//
+//}
+//
+//bool LegalEntityMaiService::importFile()
+//{
+//
+//}
+//
+//bool LegalEntityMaiService::exportFile()
+//{
+//
+//}
