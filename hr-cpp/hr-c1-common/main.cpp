c4a3be2a89d115830629515f83614f008476995a
gusha213 <2457459371@qq.com>
2023-05-22 15:10:40 +0800

    修改调试环境更改main端口

--- hr-cpp/hr-c1-common/main.cpp
+++ hr-cpp/hr-c1-common/main.cpp
@@ -43,10 +43,10 @@ bool getStartArg(int argc, char* argv[]) {
 	std::string serverPort = "8090";
 	// 数据库连接信息
 	std::string dbUsername = "root";
-	std::string dbPassword = "123456";
+	std::string dbPassword = "114514mysql";
 	std::string dbName = "test";
-	std::string dbHost = "192.168.220.128";
-	int dbPort = 3306;
+	std::string dbHost = "8.130.89.148";
+	int dbPort = 3965;
 	int dbMax = 25;
 #ifdef USE_NACOS
 	// Nacos配置参数
--- hr-cpp/hr-c1-orgmanage/controller/Router.cpp
+++ hr-cpp/hr-c1-orgmanage/controller/Router.cpp
@@ -52,7 +52,8 @@ void Router::initRouter()
 #endif
 
 	//#TIP :系统扩展路由定义，写在这个后面
-
+	createJobDownloadRouter();
+	createJobUpdateRouter();
 }
 
 #ifdef HTTP_SERVER_DEMO
