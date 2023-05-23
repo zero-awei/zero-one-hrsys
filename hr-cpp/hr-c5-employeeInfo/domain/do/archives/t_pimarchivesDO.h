74d262fcd75fa29d1d635c98fa198a1237a699ed
Yuelinfeng <2021214376@stu.cqupt.edu.cn>
2023-05-23 15:33:08 +0800

    将archivesVO更改为t_pimarchivesVO

---
+++ hr-cpp/hr-c5-employeeInfo/domain/do/archives/t_pimarchivesDO.h
@@ -0,0 +1,42 @@
+#pragma once
+#ifndef _ARCHIVES_DO_
+#define _ARCHIVES_DO_
+#include "../DoInclude.h"
+
+/**
+* 档案信息实体类
+*/
+class t_pimarchivesDO
+{
+	// 档案编号
+	CC_SYNTHESIZE(string, archivesNo, ArchivesNo);
+	// 管理单位
+	CC_SYNTHESIZE(string, managementUnits, ManagementUnits);
+	// 档案保管地
+	CC_SYNTHESIZE(string, DABGD);
+	// 档案室
+	CC_SYNTHESIZE(string, archivesCenterName, ArchivesCenterName);
+	// 员工编号
+	CC_SYNTHESIZE(string, YGBH);
+	// 员工姓名
+	CC_SYNTHESIZE(string, pimPersonName, PimPersonName);
+	// 员工状态
+	CC_SYNTHESIZE(string, ygzt);
+	// 所属单位
+	CC_SYNTHESIZE(string, ORMORGName);
+	// 档案状态
+	CC_SYNTHESIZE(string, DAZT);
+public:
+	t_pimarchivesDO() {
+		archivesNo = "";
+		managementUnits = "";
+		DABGD = "";
+		archivesCenterName = "";
+		YGBH = "";
+		pimPersonName = "";
+		ygzt = "";
+		ORMORGName = "";
+		DAZT = "";
+	}
+};
+#endif // !_ARCHIVES_DO_
