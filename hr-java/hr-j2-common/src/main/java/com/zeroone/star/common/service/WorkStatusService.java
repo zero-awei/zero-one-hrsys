77409366508087787b794999a3617c2c17fc6424
Martain-mwx <Martain699@outlook.com>
2023-05-23 23:31:46 +0800

    工作状态公共接口开发

---
+++ hr-java/hr-j2-common/src/main/java/com/zeroone/star/common/service/WorkStatusService.java
@@ -0,0 +1,19 @@
+package com.zeroone.star.common.service;
+
+import com.zeroone.star.project.dto.common.DropdownListOptionDTO;
+
+import java.util.List;
+
+/**
+ * @author mwx
+ * @version 1.0
+ * @description: TODO
+ * @date 2023/5/23 23:20
+ */
+public interface WorkStatusService {
+    /**
+     * 查询工作状态下拉列表
+     * @return
+     */
+    List<DropdownListOptionDTO> listWorkStatus();
+}
---
+++ hr-java/hr-j2-common/src/main/java/com/zeroone/star/common/service/impl/WorkStatusServiceImpl.java
@@ -0,0 +1,20 @@
+package com.zeroone.star.common.service.impl;
+
+import com.zeroone.star.common.service.WorkStatusService;
+import com.zeroone.star.project.dto.common.DropdownListOptionDTO;
+
+import java.util.List;
+
+/**
+ * @author mwx
+ * @version 1.0
+ * @description: TODO
+ * @date 2023/5/23 23:20
+ */
+public class WorkStatusServiceImpl implements WorkStatusService {
+
+    @Override
+    public List<DropdownListOptionDTO> listWorkStatus() {
+        return null;
+    }
+}
