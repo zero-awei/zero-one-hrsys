1b6cad0dda1d54af79d271085fd020c9fee703d4
Andrew <1207368051@qq.com>
2023-05-23 14:45:19 +0800

    修改证书类型SQL脚本，增加初始数据

--- documents/02、项目需求/业务参考系统/sql/credentialType.sql
+++ documents/02、项目需求/业务参考系统/sql/credentialType.sql
@@ -1,7 +1,12 @@
-CREATE TABLE `t_credentialtype` (
+-- table structure for zo_credentialtype
+CREATE TABLE `zo_credentialtype` (
     `id` VARCHAR(60) NOT NULL COMMONT '证书类型ID', 
-    `name` VARCHAR(60) NOT NULL COMMONT '证书类型名称',
+    `name` VARCHAR(60) NOT NULL COMMONT DEFAULT 'NULL' '证书类型名称',
     `code` varchar(60) NOT NULL COMMENT '证书类型代号',
     `validity` TINYINT(1) NOT NULL DEFAULT '1' COMMENT '是否有效 (0无效, 1有效)',
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8mb4_0900_ai_ci COMMENT='证书类型';
+
+-- initiate some records for zo_credentialtype
+INSERT INTO `zo_credential` VALUES ('1', '执（职）业资格证书', '10', 1);
+INSERT INTO `zo_credential` VALUES ('2', '（岗位）技能证书', '5', 1);
