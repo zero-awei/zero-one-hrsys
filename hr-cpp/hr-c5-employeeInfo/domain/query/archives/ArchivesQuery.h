17f2ef63a2cbe9b0632012cee39b4ed496959d89
lucky <2032489840@qq.com>
2023-05-23 22:10:15 +0800

    小改动1

--- hr-cpp/hr-c5-employeeInfo/domain/query/archives/ArchivesQuery.h
+++ hr-cpp/hr-c5-employeeInfo/domain/query/archives/ArchivesQuery.h
@@ -8,16 +8,12 @@
 #include OATPP_CODEGEN_BEGIN(DTO)
 
 /**
- * 示例分页查询对象
+ * 档案分页查询对象
  */
 class ArchivesQuery : public PageQuery
 {
 	DTO_INIT(ArchivesQuery, PageQuery);
-	// 姓名
-	DTO_FIELD(String, name);
-	DTO_FIELD_INFO(name) {
-		info->description = ZH_WORDS_GETTER("sample.field.name");
-	}
+
 };
 
 #include OATPP_CODEGEN_END(DTO)
