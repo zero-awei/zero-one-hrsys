b5705bade2bad645cc4ecaba93b91bdb39d57d0b
Yuelinfeng <2021214376@stu.cqupt.edu.cn>
2023-05-23 12:58:25 +0800

    新增中文词典

--- hr-cpp/hr-c5-employeeInfo/domain/dto/archives/ArchivesDTO.h
+++ hr-cpp/hr-c5-employeeInfo/domain/dto/archives/ArchivesDTO.h
@@ -15,55 +15,55 @@ class ArchivesDTO : public oatpp::DTO
 	// 档案编号，
 	DTO_FIELD(String, archivesNo);
 	DTO_FIELD_INFO(archivesNo) {
-		info->description = ZH_WORDS_GETTER("dto.field.archivesNo");
+		info->description = ZH_WORDS_GETTER("archives.field.archivesNo");
 	}
 
 	// 管理单位，
 	DTO_FIELD(String, managementUnits);
 	DTO_FIELD_INFO(managementUnits) {
-		info->description = ZH_WORDS_GETTER("dto.field.managementUnits");
+		info->description = ZH_WORDS_GETTER("archives.field.managementUnits");
 	}
 
 	// 档案保管地，
 	DTO_FIELD(String, DABGD);
 	DTO_FIELD_INFO(DABGD) {
-		info->description = ZH_WORDS_GETTER("dto.field.DABGD");
+		info->description = ZH_WORDS_GETTER("archives.field.DABGD");
 	}
 
 	// 档案室，
 	DTO_FIELD(String, archivesCenterName);
 	DTO_FIELD_INFO(archivesCenterName) {
-		info->description = ZH_WORDS_GETTER("dto.field.archivesCenterName");
+		info->description = ZH_WORDS_GETTER("archives.field.archivesCenterName");
 	}
 
 	// 员工编号，
 	DTO_FIELD(String, YGBH);
 	DTO_FIELD_INFO(YGBH) {
-		info->description = ZH_WORDS_GETTER("dto.field.YGBH");
+		info->description = ZH_WORDS_GETTER("archives.field.YGBH");
 	}
 
 	// 员工姓名，
 	DTO_FIELD(String, PimPersonName);
 	DTO_FIELD_INFO(PimPersonName) {
-		info->description = ZH_WORDS_GETTER("dto.field.PimPersonName");
+		info->description = ZH_WORDS_GETTER("archives.field.PimPersonName");
 	}
 
 	// 员工状态
 	DTO_FIELD(String, ygzt);
 	DTO_FIELD_INFO(ygzt) {
-		info->description = ZH_WORDS_GETTER("dto.field.ygzt");
+		info->description = ZH_WORDS_GETTER("archives.field.ygzt");
 	}
 
 	// 所属单位，
 	DTO_FIELD(String, ORMORGName);
 	DTO_FIELD_INFO(ORMORGName) {
-		info->description = ZH_WORDS_GETTER("dto.field.ORMORGName");
+		info->description = ZH_WORDS_GETTER("archives.field.ORMORGName");
 	}
 
 	// 档案状态，
 	DTO_FIELD(String, DAZT);
 	DTO_FIELD_INFO(DAZT) {
-		info->description = ZH_WORDS_GETTER("dto.field.DAZT");
+		info->description = ZH_WORDS_GETTER("archives.field.DAZT");
 	}
 };
 
