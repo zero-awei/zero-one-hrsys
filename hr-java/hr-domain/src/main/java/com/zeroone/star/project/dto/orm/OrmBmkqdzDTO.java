a47d60765910772b99eeeaa429dc89ffae380046
Martain-mwx <Martain699@outlook.com>
2023-05-23 23:13:21 +0800

    修改部门考勤地址

--- hr-java/hr-domain/src/main/java/com/zeroone/star/project/dto/orm/OrmBmkqdzDTO.java
+++ hr-java/hr-domain/src/main/java/com/zeroone/star/project/dto/orm/OrmBmkqdzDTO.java
@@ -4,6 +4,8 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 /**
  * @author mwx
  * @version 1.0
@@ -13,14 +15,12 @@ import lombok.Data;
 @Data
 @ApiModel("部门考勤地址对象")
 public class OrmBmkqdzDTO {
+    @NotBlank(message = "部门ID不能为空")
+    @ApiModelProperty(value = "部门唯一标识",example = "402AF7C8-B205-455D-95F3-5D7275994199")
+    private String departmentId;
 
-    @ApiModelProperty(value = "部门唯一标识",example = "1")
-    private String ormorgdzid;
 
-    @ApiModelProperty(value = "部门考勤地址唯一标识",example = "1")
-    private String ormbmkqdzid;
-
-    @ApiModelProperty(value = "部门考勤地址名",example = "XX省XX市XX区XX路XX号")
-    private String ormbmkqdzname;
+    @ApiModelProperty(value = "部门考勤地址名",example = "XX市XX区XX路XX号")
+    private String departmentkqdz;
 
 }
