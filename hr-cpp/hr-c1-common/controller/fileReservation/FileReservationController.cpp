f71e2751ebf110ae3260ffc0a47a158ca353b513
yuanxiang <wedonotfear@163.com>
2023-05-24 19:19:33 +0800

    将档案保管地和档案借阅状态正确对应到相应的code

--- hr-cpp/hr-c1-common/controller/fileReservation/FileReservationController.cpp
+++ hr-cpp/hr-c1-common/controller/fileReservation/FileReservationController.cpp
@@ -6,9 +6,9 @@
 PullListVO::Wrapper FileReservationController::execQueryFileReservation()
 {
 	auto dto = PullListDTO::createShared();
-	auto localbase = ItemDTO::createShared(1, ZH_WORDS_GETTER("fileReservation.localbase"));
-	auto enterbase = ItemDTO::createShared(2, ZH_WORDS_GETTER("fileReservation.enterbase"));
-	auto other = ItemDTO::createShared(3, ZH_WORDS_GETTER("fileReservation.other"));
+	auto localbase = ItemDTO::createShared(10, ZH_WORDS_GETTER("fileReservation.localbase"));
+	auto enterbase = ItemDTO::createShared(20, ZH_WORDS_GETTER("fileReservation.enterbase"));
+	auto other = ItemDTO::createShared(30, ZH_WORDS_GETTER("fileReservation.other"));
 	dto->pullList->push_back(localbase);
 	dto->pullList->push_back(enterbase);
 	dto->pullList->push_back(other);
--- hr-cpp/hr-c1-common/controller/fileStatus/FilestatusController.cpp
+++ hr-cpp/hr-c1-common/controller/fileStatus/FilestatusController.cpp
@@ -6,9 +6,9 @@
 PullListVO::Wrapper FileStatusController::execQueryFileStatus()
 {
 	auto dto = PullListDTO::createShared();
-	auto borrowing = ItemDTO::createShared(1, ZH_WORDS_GETTER("fileStatus.borrowing"));
+	auto borrowing = ItemDTO::createShared(10, ZH_WORDS_GETTER("fileStatus.borrowing"));
 	dto->pullList->push_back(borrowing);
-	auto returned = ItemDTO::createShared(2, ZH_WORDS_GETTER("fileStatus.returned"));
+	auto returned = ItemDTO::createShared(20, ZH_WORDS_GETTER("fileStatus.returned"));
 	dto->pullList->push_back(returned);
 	auto vo = PullListVO::createShared();
 	vo->success(dto);
