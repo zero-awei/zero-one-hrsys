e279e50a90851391d0cea9557a2442f87a1ee21e
17853318082 <87115497+17853318082@users.noreply.github.com>
2023-05-24 15:02:09 +0800

    修改jobtitle接口路径

--- hr-cpp/hr-c6-basemanage/main.cpp
+++ hr-cpp/hr-c6-basemanage/main.cpp
@@ -43,7 +43,8 @@
 	std::string serverPort = "8090";
 	// 数据库连接信息
 	std::string dbUsername = "root";
-	std::string dbPassword = "root";
+	//std::string dbPassword = "root";
+	std::string dbPassword = "123456";
 	std::string dbName = "test";
 	std::string dbHost = "127.0.0.1";
 	int dbPort = 3306;
