2f2016ac65b301bee6b4715dea569e19ece158fa
Andrew <1207368051@qq.com>
2023-05-24 19:57:36 +0800

    解决合并bug

--- hr-cpp/hr-c1-orgmanage/controller/jobSet/joboutput/JobOutputController.h
+++ hr-cpp/hr-c1-orgmanage/controller/jobSet/joboutput/JobOutputController.h
@@ -1,3 +1,6 @@
+//2023年5月21日
+//岗位导出信息
+//作者：狗皮电耗子
 #pragma once
 #ifndef _JOB_OUTPUT_CONTROLLER_
 #define _JOB_OUTPUT_CONTROLLER_
@@ -6,8 +9,8 @@
 #include "ApiHelper.h"
 #include "Macros.h"
 #include "ServerInfo.h"
-#include "domain/dto/jobSet/jobDownloadDto/JobDownloadDTO.h"
-#include "domain/vo/jobSet/JobDownloadVO/JobDownloadVO.h"
+#include "domain/dto/jobDownload/JobDownloadDTO.h"
+#include "domain/vo/JobDownload/JobDownloadVO.h"
 
 #include OATPP_CODEGEN_BEGIN(ApiController)
 
@@ -19,7 +22,7 @@ public:
 		info->summary = ZH_WORDS_GETTER("job.downloadurl");
 		API_DEF_ADD_RSP_JSON_WRAPPER(JobDownloadJsonVO);
 	}
-	ENDPOINT(API_M_GET, "/joburl", downloadJobinfo) {
+	ENDPOINT(API_M_GET, PATH_TO_JOBSET("/export-job"), downloadJobinfo) {
 		// 响应结果
 		API_HANDLER_RESP_VO(execDownloadJobinfo());
 		return createResponse(Status::CODE_202, "OK");
--- hr-cpp/hr-c1-orgmanage/controller/jobSet/jobupdate/JobUpdateController.h
+++ hr-cpp/hr-c1-orgmanage/controller/jobSet/jobupdate/JobUpdateController.h
@@ -1,3 +1,6 @@
+//2023年5月21年
+//岗位更新信息
+//作者：狗皮电耗子
 #pragma once
 #ifndef _JOB_UPDATE_CONTROLLER_
 #define _JOB_UPDATE_CONTROLLER_
@@ -6,8 +9,8 @@
 #include "Macros.h"
 #include "ServerInfo.h"
 #include "domain/vo/BaseJsonVO.h"
-#include "../../../domain/dto/jobSet/jobUpdateDto/JobUpdateDTO.h"
-#include "../../../domain/vo/jobSet/JobUpdateVO/JobUpdateVO.h"
+#include "../../../domain/dto/jobUpdate/JobUpdateDTO.h"
+#include "../../../domain/vo/jobUpdate/JobUpdateVO.h"
 
 #include OATPP_CODEGEN_BEGIN(ApiController)
 
@@ -21,7 +24,7 @@ public:
 		// 定义响应参数格式
 		API_DEF_ADD_RSP_JSON_WRAPPER(JobUpdateJsonVO);
 	}
-	ENDPOINT(API_M_PUT, "/jobset", updatejobinfo, BODY_DTO(JobUpdateDTO::Wrapper, dto)) {
+	ENDPOINT(API_M_PUT, PATH_TO_JOBSET("/update-job"), updatejobinfo, BODY_DTO(JobUpdateDTO::Wrapper, dto)) {
 		// 响应结果
 		API_HANDLER_RESP_VO(execUpdateJobinfo(dto));
 	}
