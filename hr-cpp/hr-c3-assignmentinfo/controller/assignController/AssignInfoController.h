10ba0a940a1b54043ec0bb573494c1a93638b6c0
Dawn <hikari_0928@dp.com>
2023-05-24 10:19:04 +0800

    上传分配信息的info完善

--- hr-cpp/hr-c3-assignmentinfo/controller/assignController/AssignInfoController.h
+++ hr-cpp/hr-c3-assignmentinfo/controller/assignController/AssignInfoController.h
@@ -113,66 +113,70 @@ public:
 		API_HANDLER_RESP_VO(execModifyAssignInfo(dto));
 	}
 	// [其他] 定义一个单文件上传接口
-	ENDPOINT(API_M_POST, "/upload-assignInfo", uploadFile, REQUEST(std::shared_ptr<IncomingRequest>, request)) {
-		/* 创建multipart容器 */
-		auto multipartContainer = std::make_shared<multipart::PartList>(request->getHeaders());
-		/* 创建multipart读取器 */
-		multipart::Reader multipartReader(multipartContainer.get());
-		/* 配置读取器读取表单字段 */
-		multipartReader.setPartReader("id", multipart::createInMemoryPartReader(-1 /* max-data-size */));
-		multipartReader.setPartReader("assign", multipart::createInMemoryPartReader(-1 /* max-data-size */));
-		multipartReader.setPartReader("type", multipart::createInMemoryPartReader(-1 /* max-data-size */));
-		multipartReader.setPartReader("organize", multipart::createInMemoryPartReader(-1 /* max-data-size */));
-		multipartReader.setPartReader("depart", multipart::createInMemoryPartReader(-1 /* max-data-size */));
-		multipartReader.setPartReader("job", multipart::createInMemoryPartReader(-1 /* max-data-size */));
-		multipartReader.setPartReader("post", multipart::createInMemoryPartReader(-1 /* max-data-size */));
-		multipartReader.setPartReader("start-time", multipart::createInMemoryPartReader(-1 /* max-data-size */));
-		multipartReader.setPartReader("end-time", multipart::createInMemoryPartReader(-1 /* max-data-size */));
-		/* 配置读取器读取文件到文件 */
-		multipartReader.setPartReader("file", multipart::createFilePartReader("public/static/file/test.png"));
-		/* 读取请求体中的数据 */
-		request->transferBody(&multipartReader);
-		/* 打印part数量 */
-		OATPP_LOGD("Multipart", "parts_count=%d", multipartContainer->count());
-		/* 获取表单数据 */
-		auto id = multipartContainer->getNamedPart("id");
-		auto assign = multipartContainer->getNamedPart("assign");
-		auto etype = multipartContainer->getNamedPart("Etype");
-		auto organize = multipartContainer->getNamedPart("organize");
-		auto depart = multipartContainer->getNamedPart("depart");
-		auto job = multipartContainer->getNamedPart("job");
-		auto post = multipartContainer->getNamedPart("post");
-		auto startTime = multipartContainer->getNamedPart("startTime");
-		auto endTime = multipartContainer->getNamedPart("endTime");
-		/* 断言表单数据是否正确 */
-		OATPP_ASSERT_HTTP(id, Status::CODE_400, "id is null");
-		OATPP_ASSERT_HTTP(assign, Status::CODE_400, "assign is null");
-		OATPP_ASSERT_HTTP(etype, Status::CODE_400, "etype is null");
-		OATPP_ASSERT_HTTP(organize, Status::CODE_400, "organize is null");
-		OATPP_ASSERT_HTTP(depart, Status::CODE_400, "depart is null");
-		OATPP_ASSERT_HTTP(job, Status::CODE_400, "job is null");
-		OATPP_ASSERT_HTTP(post, Status::CODE_400, "post is null");
-		OATPP_ASSERT_HTTP(startTime, Status::CODE_400, "startTime is null");
-		OATPP_ASSERT_HTTP(endTime, Status::CODE_400, "endTime is null");
-		/* 打印应表单数据 */
-		OATPP_LOGD("Multipart", "id='%s'", id->getPayload()->getInMemoryData()->c_str());
-		OATPP_LOGD("Multipart", "assign='%s'", assign->getPayload()->getInMemoryData()->c_str());
-		OATPP_LOGD("Multipart", "etype='%s'", etype->getPayload()->getInMemoryData()->c_str());
-		OATPP_LOGD("Multipart", "organize='%s'", organize->getPayload()->getInMemoryData()->c_str());
-		OATPP_LOGD("Multipart", "depart='%s'", depart->getPayload()->getInMemoryData()->c_str());
-		OATPP_LOGD("Multipart", "job='%s'", job->getPayload()->getInMemoryData()->c_str());
-		OATPP_LOGD("Multipart", "post='%s'", post->getPayload()->getInMemoryData()->c_str());
-		OATPP_LOGD("Multipart", "startTime='%s'", startTime->getPayload()->getInMemoryData()->c_str());
-		OATPP_LOGD("Multipart", "endTime='%s'", endTime->getPayload()->getInMemoryData()->c_str());
-		/* 获取文件部分 */
-		auto filePart = multipartContainer->getNamedPart("file");
-		/* 断言文件是否获取到 */
-		OATPP_ASSERT_HTTP(filePart, Status::CODE_400, "file is null");
-		/* 打印文件名称 */
-		OATPP_LOGD("Multipart", "file='%s'", filePart->getFilename()->c_str());
-		/* 响应OK */
-		return createResponse(Status::CODE_200, "OK");
+	ENDPOINT_INFO(importAssignInfo) {
+		info->summary = ZH_WORDS_GETTER("employee.post.upload");
+		API_DEF_ADD_RSP_JSON_WRAPPER(Uint64JsonVO);
 	}
+		ENDPOINT(API_M_POST, "/upload-assignInfo", importAssignInfo, REQUEST(std::shared_ptr<IncomingRequest>, request)) {
+			/* 创建multipart容器 */
+			auto multipartContainer = std::make_shared<multipart::PartList>(request->getHeaders());
+			/* 创建multipart读取器 */
+			multipart::Reader multipartReader(multipartContainer.get());
+			/* 配置读取器读取表单字段 */
+			multipartReader.setPartReader("id", multipart::createInMemoryPartReader(-1 /* max-data-size */));
+			multipartReader.setPartReader("assign", multipart::createInMemoryPartReader(-1 /* max-data-size */));
+			multipartReader.setPartReader("type", multipart::createInMemoryPartReader(-1 /* max-data-size */));
+			multipartReader.setPartReader("organize", multipart::createInMemoryPartReader(-1 /* max-data-size */));
+			multipartReader.setPartReader("depart", multipart::createInMemoryPartReader(-1 /* max-data-size */));
+			multipartReader.setPartReader("job", multipart::createInMemoryPartReader(-1 /* max-data-size */));
+			multipartReader.setPartReader("post", multipart::createInMemoryPartReader(-1 /* max-data-size */));
+			multipartReader.setPartReader("start-time", multipart::createInMemoryPartReader(-1 /* max-data-size */));
+			multipartReader.setPartReader("end-time", multipart::createInMemoryPartReader(-1 /* max-data-size */));
+			/* 配置读取器读取文件到文件 */
+			multipartReader.setPartReader("file", multipart::createFilePartReader("public/static/file/test.png"));
+			/* 读取请求体中的数据 */
+			request->transferBody(&multipartReader);
+			/* 打印part数量 */
+			OATPP_LOGD("Multipart", "parts_count=%d", multipartContainer->count());
+			/* 获取表单数据 */
+			auto id = multipartContainer->getNamedPart("id");
+			auto assign = multipartContainer->getNamedPart("assign");
+			auto etype = multipartContainer->getNamedPart("Etype");
+			auto organize = multipartContainer->getNamedPart("organize");
+			auto depart = multipartContainer->getNamedPart("depart");
+			auto job = multipartContainer->getNamedPart("job");
+			auto post = multipartContainer->getNamedPart("post");
+			auto startTime = multipartContainer->getNamedPart("startTime");
+			auto endTime = multipartContainer->getNamedPart("endTime");
+			/* 断言表单数据是否正确 */
+			OATPP_ASSERT_HTTP(id, Status::CODE_400, "id is null");
+			OATPP_ASSERT_HTTP(assign, Status::CODE_400, "assign is null");
+			OATPP_ASSERT_HTTP(etype, Status::CODE_400, "etype is null");
+			OATPP_ASSERT_HTTP(organize, Status::CODE_400, "organize is null");
+			OATPP_ASSERT_HTTP(depart, Status::CODE_400, "depart is null");
+			OATPP_ASSERT_HTTP(job, Status::CODE_400, "job is null");
+			OATPP_ASSERT_HTTP(post, Status::CODE_400, "post is null");
+			OATPP_ASSERT_HTTP(startTime, Status::CODE_400, "startTime is null");
+			OATPP_ASSERT_HTTP(endTime, Status::CODE_400, "endTime is null");
+			/* 打印应表单数据 */
+			OATPP_LOGD("Multipart", "id='%s'", id->getPayload()->getInMemoryData()->c_str());
+			OATPP_LOGD("Multipart", "assign='%s'", assign->getPayload()->getInMemoryData()->c_str());
+			OATPP_LOGD("Multipart", "etype='%s'", etype->getPayload()->getInMemoryData()->c_str());
+			OATPP_LOGD("Multipart", "organize='%s'", organize->getPayload()->getInMemoryData()->c_str());
+			OATPP_LOGD("Multipart", "depart='%s'", depart->getPayload()->getInMemoryData()->c_str());
+			OATPP_LOGD("Multipart", "job='%s'", job->getPayload()->getInMemoryData()->c_str());
+			OATPP_LOGD("Multipart", "post='%s'", post->getPayload()->getInMemoryData()->c_str());
+			OATPP_LOGD("Multipart", "startTime='%s'", startTime->getPayload()->getInMemoryData()->c_str());
+			OATPP_LOGD("Multipart", "endTime='%s'", endTime->getPayload()->getInMemoryData()->c_str());
+			/* 获取文件部分 */
+			auto filePart = multipartContainer->getNamedPart("file");
+			/* 断言文件是否获取到 */
+			OATPP_ASSERT_HTTP(filePart, Status::CODE_400, "file is null");
+			/* 打印文件名称 */
+			OATPP_LOGD("Multipart", "file='%s'", filePart->getFilename()->c_str());
+			/* 响应OK */
+			return createResponse(Status::CODE_200, "OK");
+		}
 
 	// 3.1 定义查询接口描述
 	ENDPOINT_INFO(assignQueryDetail) {
