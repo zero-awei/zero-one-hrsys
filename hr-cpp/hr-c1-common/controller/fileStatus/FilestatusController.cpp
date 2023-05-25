580f094422527f1981ef6959b7b2f1af62c3d1f6
yuanxiang <wedonotfear@163.com>
2023-05-25 20:33:15 +0800

    在离职、解聘、档案借阅状态、档案保管地下拉列表中加入缓存

--- hr-cpp/hr-c1-common/controller/fileStatus/FilestatusController.cpp
+++ hr-cpp/hr-c1-common/controller/fileStatus/FilestatusController.cpp
@@ -1,15 +1,35 @@
 
 #include "stdafx.h"
 #include "FileStatusController.h"
-
+#include "uselib/pullListRedis/UseLibRedis.h"
 
 PullListVO::Wrapper FileStatusController::execQueryFileStatus()
 {
 	auto dto = PullListDTO::createShared();
-	auto borrowing = ItemDTO::createShared(10, ZH_WORDS_GETTER("fileStatus.borrowing"));
-	dto->pullList->push_back(borrowing);
-	auto returned = ItemDTO::createShared(20, ZH_WORDS_GETTER("fileStatus.returned"));
-	dto->pullList->push_back(returned);
+	UseLibRedis redisExm;
+	std::string tableName = "file-status-pull-list";
+	std::unordered_map<std::string, std::string> fileStatusList;
+	std::unordered_map<std::string, std::string> redisResult = redisExm.queryRedis(tableName);
+	if (redisResult.empty()) {
+		// 列表数据
+		auto borrowing = ItemDTO::createShared(10, ZH_WORDS_GETTER("fileStatus.borrowing"));
+		auto returned = ItemDTO::createShared(20, ZH_WORDS_GETTER("fileStatus.returned"));
+		dto->pullList->push_back(borrowing);
+		dto->pullList->push_back(returned);
+
+		// 加入缓存
+		for (auto subptr = dto->pullList->begin(); subptr != dto->pullList->end(); subptr++) {
+			fileStatusList.insert(std::make_pair(std::to_string(*subptr->get()->key), *subptr->get()->val));
+		}
+		// 加入缓存
+		redisExm.updateRedisWithTable(fileStatusList, tableName);
+	}
+	else { // 缓存有数据
+		fileStatusList = redisExm.queryRedis(tableName);
+		for (auto sub : fileStatusList) {
+			dto->pullList->push_back(ItemDTO::createShared(std::stoi(sub.first), sub.second));
+		}
+	}
 	auto vo = PullListVO::createShared();
 	vo->success(dto);
 	return vo;
