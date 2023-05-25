6ac8690b49a0b0c7afaaef9b6a48451bbfe7a32d
pine <1720674172@qq.com>
2023-05-25 22:33:50 +0800

    :修改了[200~#pragma once

--- hr-cpp/hr-c2-basmanager/domain/do/CertificateManage/CertificateDO.h
+++ hr-cpp/hr-c2-basmanager/domain/do/CertificateManage/CertificateDO.h
@@ -1,41 +1,57 @@
 #pragma once
-//证书管理数据库实体类
+/*
+ Copyright Zero One Star. All rights reserved.
 
+ @Author: awei
+ @Date: 2022/10/25 11:52:32
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+	  https://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
 #ifndef _CERTIFICATE_DO_
 #define _CERTIFICATE_DO_
 #include "../DoInclude.h"
 
-/**
- * 证书管理数据库实体类
- */
-class CertificateDO 
+class CertificateDO
 {
-	// 员工基本信息表ID,同员工ID
-	CC_SYNTHESIZE(uint64_t, ygbh, ygbh);
+	/**
+	 * 证书管理数据库实体类
+	 */
+	 // 员工基本信息表ID,同员工ID
+	CC_SYNTHESIZE(uint64_t, ygbh, Ygbh);
 	// 姓名
-	CC_SYNTHESIZE(string, pimperSonName, pimperSonName);
+	CC_SYNTHESIZE(string, pimperSonName, PimperSonName);
 	// 员工状态 
-	CC_SYNTHESIZE(string, ygzt, ygzt);
+	CC_SYNTHESIZE(string, ygzt, Ygzt);
 	// 组织   t_srforg 需要连表操作获取组织名称
-	CC_SYNTHESIZE(string, organizationName, organizationName);
+	CC_SYNTHESIZE(string, organizationName, OrganizationName);
 	// 证书编号
-	CC_SYNTHESIZE(string, zgzsbh, zgzsbh);
+	CC_SYNTHESIZE(string, zgzsbh, Zgzsbh);
 	// 一建配套B证编号
-	CC_SYNTHESIZE(string, bcardNumber, bcardNumber);
+	CC_SYNTHESIZE(string, bcardNumber, BcardNumber);
 	// 证书名称
-	CC_SYNTHESIZE(string, pimVocationalName, pimVocationalName);
+	CC_SYNTHESIZE(string, pimVocationalName, PimVocationalName);
 	// 证书类型
-	CC_SYNTHESIZE(string, zslx, zslx);
+	CC_SYNTHESIZE(string, zslx, Zslx);
 	// 签发日期
-	CC_SYNTHESIZE(string, zghqrq, zghqrq);
+	CC_SYNTHESIZE(string, zghqrq, Zghqrq);
 	// 签发机构
-	CC_SYNTHESIZE(string, zgsydw, zgsydw);
+	CC_SYNTHESIZE(string, zgsydw, Zgsydw);
 	// 注册单位
-	CC_SYNTHESIZE(string, zcdw, zcdw);
+	CC_SYNTHESIZE(string, zcdw, Zcdw);
 	// 发证有效期
-	CC_SYNTHESIZE(string, fzyxq, fzyxq);
+	CC_SYNTHESIZE(string, fzyxq, Fzyxq);
 	// 失效日期
-	CC_SYNTHESIZE(string, sxrq, sxrq);
+	CC_SYNTHESIZE(string, sxrq, Sxrq);
 	// 证书剩余日期 表内没有
 	//////////////////////////////////////////////////////////////
 	// 以下为非前端显示字段
@@ -43,8 +59,24 @@ class CertificateDO
 	CC_SYNTHESIZE(string, pimPersonID, pimPersonID);
 	// 组织标识 FOREIGN KEY (`ORMORGID`) REFERENCES `t_srforg` (`ORGID`),
 	CC_SYNTHESIZE(string, ORMORGID, ORMORGID);
+
+
 public:
-	CertificateDO() {	}
+	CertificateDO() {
+		ygbh = 0;
+		pimperSonName = "";
+		ygzt = "";
+		organizationName = "";
+		zgzsbh = "";
+		bcardNumber = "";
+		pimVocationalName = "";
+		zslx = "";
+		zghqrq = "";
+		zgsydw = "";
+		zcdw = -1;
+		fzyxq = "";
+		sxrq = "";
+	}
 };
 
-#endif 
+#endif // !_FPRMERRMPLOYEES_DO_
