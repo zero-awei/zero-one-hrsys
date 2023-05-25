2926189ad4c176bbdc3b2cfd0ab3106e7b477c5b
jiang <yslonn@qq.com>
2023-05-26 02:32:11 +0800

    jiang：更改t_pimpatentDO.h中大小写符合高保真模型字段

--- hr-cpp/hr-c5-employeeInfo/domain/do/patentinfo/t_pimpatentDO.h
+++ hr-cpp/hr-c5-employeeInfo/domain/do/patentinfo/t_pimpatentDO.h
@@ -9,56 +9,56 @@
 class PatentinfoDO
 {
     //ZLH:专利号
-    CC_SYNTHESIZE(string, ZLH, ZLH);
+    CC_SYNTHESIZE(string, zlh, ZLH);
     //UPDATEDATE : 更新时间
-    CC_SYNTHESIZE(string, UPDATEDATE, UPDATEDATE);
+    CC_SYNTHESIZE(string, updatedate, UPDATEDATE);
     //PIMPATENTID : 专利信息编码
-    CC_SYNTHESIZE(string, PIMPATENTID, PIMPATENTID);
+    CC_SYNTHESIZE(string, pimpatentid, PIMPATENTID);
     //ZLHQSJ : 专利获取时间
-    CC_SYNTHESIZE(string, ZLHQSJ, ZLHQSJ);
+    CC_SYNTHESIZE(string, zlhqsj, ZLHQSJ);
     //PIMPATENTNAME : 专利名称
-    CC_SYNTHESIZE(string, PIMPATENTNAME, PIMPATENTNAME);
+    CC_SYNTHESIZE(string, pimpatentname, PIMPATENTNAME);
     //UPDATEMAN : 更新人
-    CC_SYNTHESIZE(string, UPDATEMAN, UPDATEMAN);
+    CC_SYNTHESIZE(string, updateman, UPDATEMAN);
     //CREATEMAN : 建立人
-    CC_SYNTHESIZE(string, CREATEMAN, CREATEMAN);
+    CC_SYNTHESIZE(string, createman, CREATEMAN);
     //CREATEDATE : 建立时间
-    CC_SYNTHESIZE(string, CREATEDATE, CREATEDATE);
+    CC_SYNTHESIZE(string, createdate, CREATEDATE);
     //ZLPZGB : 专利批准国别
-    CC_SYNTHESIZE(string, ZLPZGB, ZLPZGB);
+    CC_SYNTHESIZE(string, zlpzgb, ZLPZGB);
     //PIMPERSONID : 人员信息标识
-    CC_SYNTHESIZE(string, PIMPERSONID, PIMPERSONID);
+    CC_SYNTHESIZE(string, pimpersonid, PIMPERSONID);
     //JLSS : 记录所属
-    CC_SYNTHESIZE(string, JLSS, JLSS);
+    CC_SYNTHESIZE(string, jlss, JLSS);
     //JLSPZT : 审批状态
-    CC_SYNTHESIZE(string, JLSPZT, JLSPZT);
+    CC_SYNTHESIZE(string, jlspzt, JLSPZT);
     //JLGLBH : 记录管理编号
-    CC_SYNTHESIZE(string, JLGLBH, JLGLBH);
+    CC_SYNTHESIZE(string, jlglbh, JLGLBH);
     //JLCZZ : 记录操作者
-    CC_SYNTHESIZE(string, JLCZZ, JLCZZ);
+    CC_SYNTHESIZE(string, jlczz, JLCZZ);
     //ENCLOLURE : 附件
-    CC_SYNTHESIZE(string, ENCLOLURE, ENCLOLURE);
+    CC_SYNTHESIZE(string, enclolure, ENCLOLURE);
     //REASON : 拒绝原因
-    CC_SYNTHESIZE(string, REASON, REASON);
+    CC_SYNTHESIZE(string, reason, REASON);
 public:
     PatentinfoDO() {
 
-        ZLH = "";
-        UPDATEDATE = "";
-        PIMPATENTID = "";
-        ZLHQSJ = "";
-        PIMPATENTNAME = "";
-        UPDATEMAN = "";
-        CREATEMAN = "";
-        CREATEDATE = "";
-        ZLPZGB = "";
-        PIMPERSONID = "";
-        JLSS = "";
-        JLSPZT = "";
-        JLGLBH = "";
-        JLCZZ = "";
-        ENCLOLURE = "";
-        REASON = "";
+        zlh = "";
+        updatedate = "";
+        pimpatentid = "";
+        zlhqsj = "";
+        pimpatentname = "";
+        updateman = "";
+        createman = "";
+        createdate = "";
+        zlpzgb = "";
+        pimpersonid = "";
+        jlss = "";
+        jlspzt = "";
+        jlglbh = "";
+        jlczz = "";
+        enclolure = "";
+        reason = "";
     }
 };
 
