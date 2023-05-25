8246ddcc660c5e895a2473b0461fd0f28e006989
FUGGGG <525403823@qq.com>
2023-05-25 15:56:33 +0800

    合并detachmentDO

--- hr-cpp/hr-c3-employeeinfo/domain/do/employeeInfo/EmployeeInfoDO.h
+++ hr-cpp/hr-c3-employeeinfo/domain/do/employeeInfo/EmployeeInfoDO.h
@@ -27,10 +27,66 @@
 class EmployeeInfoDO
 {
 protected:
+	CC_SYNTHESIZE(string, id, Id)
 	CC_SYNTHESIZE(string, name, Name)
+	CC_SYNTHESIZE(string, idType, IdType)
+	CC_SYNTHESIZE(string, idNum, IdNum)
+	CC_SYNTHESIZE(string, birthday, Birthday)
+	CC_SYNTHESIZE(int, age, Age)
+	CC_SYNTHESIZE(string, sex, Sex)
+	CC_SYNTHESIZE(string, blood, Blood)
+	CC_SYNTHESIZE(string, nation, Nation)
+	CC_SYNTHESIZE(string, native, Native)
+	CC_SYNTHESIZE(string, residentPlace, ResidentPlace)
+	CC_SYNTHESIZE(string, residentLocate, ResidentLocate)
+	CC_SYNTHESIZE(string, birthPlace, BirthPlace)
+	CC_SYNTHESIZE(string, comePlace, ComePlace)
+	CC_SYNTHESIZE(string, onlyCredit, OnlyCredit)
+	CC_SYNTHESIZE(string, hobby, Hobby)
+	CC_SYNTHESIZE(string, health, Health)
+	CC_SYNTHESIZE(string, marriage, Marriage)
+	CC_SYNTHESIZE(string, politic, Politic)
+	CC_SYNTHESIZE(string, rank, Rank)
+	CC_SYNTHESIZE(string, workTime, WorkTime)
+	CC_SYNTHESIZE(string, inTime, InTime)
+	CC_SYNTHESIZE(string, email, Email)
+	CC_SYNTHESIZE(string, techTitle, TechTitle)
+	CC_SYNTHESIZE(string, acaCredit, AcaCredit)
+	CC_SYNTHESIZE(string, firAcaCredit, FirAcaCredit)
+	CC_SYNTHESIZE(string, pracCerfigicate, PracCerfigicate)
+	CC_SYNTHESIZE(string, photo, Photo)
+	CC_SYNTHESIZE(string, phone, Phone)
 public:
 	EmployeeInfoDO() {
+		id = "";
 		name = "";
+		idType = "";
+		idNum = "";
+		birthday = "";
+		age = 0;
+		sex = "";
+		blood = "";
+		nation = "";
+		native = "";
+		residentPlace = "";
+		residentLocate = "";
+		birthPlace = "";
+		comePlace = "";
+		onlyCredit = "";
+		hobby = "";
+		health = "";
+		marriage = "";
+		politic = "";
+		rank = "";
+		workTime = "";
+		inTime = "";
+		email = "";
+		techTitle = "";
+		acaCredit = "";
+		firAcaCredit = "";
+		pracCerfigicate = "";
+		phone = "";
+		photo = "";
 	}
 };
 
