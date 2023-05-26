27834a23cf14c708e10e4d1ccee3686cbdb7babc
LZF <lzf847490420@163.com>
2023-05-27 02:02:32 +0800

    Revert "Delete A.vue"
    This reverts commit d37987011cb2cfe82fcba7d002f010af911d0127.

---
+++ hr-frontend/src/views/sample/Communication/A.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hr-btn" @click="toB">A组件</div>
+</template>
+
+<script setup>
+const mitt = getCurrentInstance().appContext.config.globalProperties.$bus
+// 给B发消息
+let val = ref('hello B')
+const toB = () => {
+  mitt.emit('callB', val.value)
+}
+
+// 绑定触发函数
+const fn = (val) => {
+  console.log(`output->A`, val)
+}
+mitt.on('callA', fn)
+</script>
+
+<style lang="scss" scoped>
+@include block(btn) {
+  width: 100px;
+  // height: 20px;
+  padding: 5px 10px;
+  text-align: center;
+  background-color: #e67e22;
+}
+</style>
