64f1875f3e5523b237f50d8afa3b531f50bdc80d
liubao <liub688@163.com>
2023-05-27 10:19:47 +0800

    feat: 完成首页样式渲染

--- hr-frontend/src/router/main/basemanage/index.js
+++ hr-frontend/src/router/main/basemanage/index.js
@@ -1,3 +1,9 @@
-const routes = []
+const routes = [
+  {
+    path: '/pimportalview',
+    name: '门户首页',
+    component: () => import('@/views/baseManage/Portal.vue')
+  }
+]
 
 export default routes
