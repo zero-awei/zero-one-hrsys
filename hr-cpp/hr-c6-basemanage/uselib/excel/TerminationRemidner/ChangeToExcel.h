0f384e6ff4cfc3e6ac4954c8bc5a9cbc1eb9ae29
zhaozheng <2926651158@qq.com>
2023-05-28 23:38:02 +0800

    终止提醒接口（写算法）

---
+++ hr-cpp/hr-c6-basemanage/uselib/excel/TerminationRemidner/ChangeToExcel.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+#include "ExcelComponent.h"
+#include "CharsetConvertHepler.h"
+#include "uselib/fastdfs/TerminationReminder/TerminationRemidnerFastdfs.h"
+#include <vector>
+using namespace std;
+class ChangeToExcel
+{
+public:
+	static string changeToExcel(vector<vector<string>>&data);
+};
