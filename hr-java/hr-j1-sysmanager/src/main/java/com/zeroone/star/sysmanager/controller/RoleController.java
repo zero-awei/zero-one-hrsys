44e77c755b64d633f37e83ae19712c84068c27ef
Alan <gcAlan@163.com>
2023-05-29 00:26:00 +0800

    角色管理接口实现，第一次提交

--- hr-java/hr-j1-sysmanager/src/main/java/com/zeroone/star/sysmanager/controller/RoleController.java
+++ hr-java/hr-j1-sysmanager/src/main/java/com/zeroone/star/sysmanager/controller/RoleController.java
@@ -3,9 +3,12 @@ package com.zeroone.star.sysmanager.controller;
 import com.zeroone.star.project.dto.sysmanager.rolemanager.RoleDTO;
 import com.zeroone.star.project.sysmanager.RoleApis;
 import com.zeroone.star.project.vo.JsonVO;
+import com.zeroone.star.sysmanager.service.IRoleService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
 
 /**
  * <p>
@@ -20,29 +23,53 @@ import org.springframework.web.bind.annotation.*;
 @Api(tags="角色管理")
 @RequestMapping("/roles")
 public class RoleController implements RoleApis {
+    @Resource
+    IRoleService iRoleService;
+
 
     @GetMapping("/query-one")
-    @ApiOperation(value = "查看管理员详情")
+    @ApiOperation(value = "查看角色详情")
     @Override
     public JsonVO<RoleDTO> queryById(String id) {
-        return null;
+        Integer roleId = Integer.valueOf(id);
+        return JsonVO.success(iRoleService.querRoleById(roleId));
     }
     @ApiOperation(value = "删除角色")
     @DeleteMapping("/delete")
     @Override
     public JsonVO<Boolean> deleteRole(String id) {
+        Integer roleId = Integer.valueOf(id);
+        boolean result = iRoleService.deleRoleById(roleId);
+
         return null;
     }
     @ApiOperation(value = "增加角色")
     @PostMapping("/add-one")
     @Override
     public JsonVO<Boolean> addOneRole(RoleDTO dto) {
+        if (dto.getId()==null&&
+            dto.getDescription()==null&&
+            dto.getName()==null&&
+            dto.getKeyword()==null){
+            return  null;
+
+        }
+        Boolean addResult = iRoleService.addRole(dto);
         return null;
     }
     @ApiOperation(value = "修改角色")
     @PutMapping("/modify")
     @Override
     public JsonVO<Boolean> modifyRole(RoleDTO dto) {
+        //输入的id不能为空
+        if (dto.getId()==null){
+            return null;
+        }
+        //角色描述字数限制
+        if(dto.getDescription().length()>50){
+            return null;
+        }
+       Boolean modifyResult= iRoleService.modifyRole(dto);
         return null;
     }
 
--- hr-java/hr-j1-sysmanager/src/main/java/com/zeroone/star/sysmanager/service/IRoleService.java
+++ hr-java/hr-j1-sysmanager/src/main/java/com/zeroone/star/sysmanager/service/IRoleService.java
@@ -1,5 +1,6 @@
 package com.zeroone.star.sysmanager.service;
 
+import com.zeroone.star.project.dto.sysmanager.rolemanager.RoleDTO;
 import com.zeroone.star.sysmanager.entity.Role;
 import com.baomidou.mybatisplus.extension.service.IService;
 
@@ -12,5 +13,34 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2023-05-28
  */
 public interface IRoleService extends IService<Role> {
+    /**
+     * 查询一个角色
+     * @param id
+     * @return role
+     */
+    RoleDTO querRoleById(Integer id);
+
+    /**
+     * 通过id删除一个角色
+     * @param id
+     * @return boolean
+     */
+    Boolean deleRoleById(Integer id);
+
+    /**
+     * 新增一个角色
+     * @param roleDTO
+     * @return boolean
+     */
+    Boolean addRole(RoleDTO roleDTO);
+
+    /**
+     * 修改一个角色
+     * @param roleDTO
+     * @return boolean
+     */
+    Boolean modifyRole(RoleDTO roleDTO);
+
+
 
 }
