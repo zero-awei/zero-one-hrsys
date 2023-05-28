13fac2097bac28db2fe25ed790525feb77c58c20
yuanxiang <wedonotfear@163.com>
2023-05-28 21:03:19 +0800

    将新增项目标签的DAO中的executeInsert修改为executeUpdate

--- hr-cpp/hr-c1-orgmanage/dao/projTag/ProjTagDAO.cpp
+++ hr-cpp/hr-c1-orgmanage/dao/projTag/ProjTagDAO.cpp
@@ -70,7 +70,7 @@ uint64_t ProjTagDAO::insert(const ProjTagDO& iObj)
 	string sql = "INSERT INTO `t_ormxmbq` \
 				(`ORMXMBQID`, `CREATEMAN`, `ORMXMBQNAME`, `UPDATEMAN`, `CREATEDATE`, `UPDATEDATE`, `ORMORGID`)\
 				 VALUES (?, ?, ?, ?, ?, ?, ?)";
-	return sqlSession->executeInsert(sql, "%s%s%s%s%s%s%s", \
+	return sqlSession->executeUpdate(sql, "%s%s%s%s%s%s%s", \
 				iObj.getId(), iObj.getCreator(), iObj.getTagName(), \
 				iObj.getUpdater(), iObj.getCreateTime(), iObj.getUpdateTime(), iObj.getOrgId());
 }
