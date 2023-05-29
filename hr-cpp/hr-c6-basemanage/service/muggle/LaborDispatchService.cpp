564646d483e27cd1491e87d7e681997ae1e4402c
Menghuan <760875524@qq.com>
2023-05-29 13:20:24 +0800

    1

---
+++ hr-cpp/hr-c6-basemanage/service/muggle/LaborDispatchService.cpp
@@ -0,0 +1,42 @@
+/*
+ Copyright Muggle. All rights reserved.
+
+ @Author: Muggle
+ @Date: 2023/05/20 0:17:17
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+	  https://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+#include "stdafx.h"
+#include "domain/query/LaborDispatch/LaborDispatchQuery-mg.h"
+#include "domain/dto/LaborDispatch/LaborDispatchDTO-mg.h"
+
+// LaborDispatchMDTO::Wrapper LaborDispatchMService::ListData(const LaborDispatchMQuery::Wrapper& query)
+// {
+// 	auto reply = LaborDispatchMDTO::createShared();
+// 	LaborDispatchMDAO dao;
+// 	auto result = dao.selectByCorporateName(query);
+// 	reply->corporateName = result.getCorporateName();
+// 	reply->corporateAddress = result.getCorporateAddress();
+// 	reply->corporateContact = result.getCorporateContact();
+// 	reply->corporateNumber = result.getCorporateNumber();
+	//reply->corporateAddress = "cosmic";
+	//reply->corporateContact = "muggle";
+	//reply->corporateName = "zerone";
+	//reply->corporateNumber = "8848";
+// 	return reply;
+// }
+// 
+// bool LaborDispatchMService::modifyData(const LaborDispatchMDTO::Wrapper& dto)
+// {
+// 	return true;//啊对对对对
+// }
