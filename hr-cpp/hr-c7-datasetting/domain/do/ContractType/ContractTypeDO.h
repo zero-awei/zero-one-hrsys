55eccb9e37974026a5478c10618b073de6641b26
kling0 <3516455668@qq.com>
2023-05-31 00:34:04 +0800

    已合并追翼kling阿未华仔的分支

--- hr-cpp/hr-c7-datasetting/domain/do/ContractType/ContractTypeDO.h
+++ hr-cpp/hr-c7-datasetting/domain/do/ContractType/ContractTypeDO.h
@@ -4,22 +4,19 @@
 
 #include "../DoInclude.h"
 
-class ContractDO
+class ContractTypeDO
 {
 	CC_SYNTHESIZE(string, id, Id);
 	CC_SYNTHESIZE(string, name, Name);
-	CC_SYNTHESIZE(string, department, Department);
-	CC_SYNTHESIZE(string, state, State);
-	CC_SYNTHESIZE(string, unitArrivalTime, UnitArrivalTime);
-	CC_SYNTHESIZE(string, contractId, ContractId);
-	CC_SYNTHESIZE(string, signingUnit, SigningUnit);
-	CC_SYNTHESIZE(string, contract_category, Contract_category);
-	CC_SYNTHESIZE(string, contract_type, Contract_type);
-	CC_SYNTHESIZE(string, startDate, StartDate);
-	CC_SYNTHESIZE(string, endDate, EndDate);
-	CC_SYNTHESIZE(string, contractStatus, ContractStatus);
-	CC_SYNTHESIZE(string, remainingDays, RemainingDays);
-	CC_SYNTHESIZE(string, expiryDate, ExpiryDate);
+public:
+	ContractTypeDO() {
+		id = "";
+		name = "";
+	}
+	ContractTypeDO(string id, string name) {
+		this->id = id;
+		this->name = name;
+	}
 };
 
 #endif // !_CONTRACTDO_H_
