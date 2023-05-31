6b86972f3707a2ed26170380dea8afa8932a2c87
jiang <yslonn@qq.com>
2023-05-31 11:32:53 +0800

    修改查询结果

--- hr-cpp/hr-c5-employeeInfo/dao/patentinfo/PatentinfoDAO.cpp
+++ hr-cpp/hr-c5-employeeInfo/dao/patentinfo/PatentinfoDAO.cpp
@@ -23,84 +23,97 @@ std::list<PatentinfoDO> PatentinfoDAO::selectByPIMPATENTID(const string& pimpate
 int PatentinfoDAO::update(const PatentinfoDO& uObj)
 {
 
-
-
-
 	//stringstream sql;
 	//stringstream fmt;
 	//sql << "UPDATE t_pimpatent SET ";
-	//if (uObj.getZLH() != "string") {
-	//	sql << "ZLH = ?";
-	//	fmt << "%s";
-	//}
-	//if (uObj.getUPDATEDATE() != "string") {
-	//	sql << "UPDATEDATE = ?";
-	//	fmt << "%s";
-	//}
-	//if (uObj.getZLHQSJ() != "string") {
-	//	sql << "UPDATEDATE = ?";
-	//	fmt << "%s";
-	//}
-	//if (uObj.getPIMPATENTNAME() != "string") {
-	//	sql << "UPDATEDATE = ?";
-	//	fmt << "%s";
-	//}
-	//if (uObj.getUPDATEMAN() != "string") {
-	//	sql << "UPDATEDATE = ?";
-	//	fmt << "%s";
-	//}
-	//if (uObj.getCREATEMAN() != "string") {
-	//	sql << "UPDATEDATE = ?";
-	//	fmt << "%s";
-	//}
-	//if (uObj.getCREATEDATE() != "string") {
-	//	sql << "UPDATEDATE = ?";
-	//	fmt << "%s";
-	//}
-	//if (uObj.getZLPZGB() != "string") {
-	//	sql << "UPDATEDATE = ?";
-	//	fmt << "%s";
-	//}
-	//if (uObj.getPIMPERSONID() != "string") {
-	//	sql << "UPDATEDATE = ?";
-	//	fmt << "%s";
-	//}
-	//if (uObj.getJLSS() != "string") {
-	//	sql << "UPDATEDATE = ?";
-	//	fmt << "%s";
-	//}
-	//if (uObj.getJLSPZT() != "string") {
-	//	sql << "UPDATEDATE = ?";
-	//	fmt << "%s";
-	//}
-	//if (uObj.getJLGLBH() != "string") {
-	//	sql << "UPDATEDATE = ?";
-	//	fmt << "%s";
-	//}
-	//if (uObj.getJLCZZ() != "string") {
-	//	sql << "UPDATEDATE = ?";
-	//	fmt << "%s";
-	//}
-	//if (uObj.getENCLOLURE() != "string") {
-	//	sql << "UPDATEDATE = ?";
-	//	fmt << "%s";
-	//}
-	//if (uObj.getREASON() != "string") {
-	//	sql << "UPDATEDATE = ?";
-	//	fmt << "%s";
-	//}
+	
 	//sql << " WHERE PIMPATENTID = ?";
 
 
 	//string sqlStr = sql.str();
 
-
-
-
-
 	//string sqlfmtStr = fmt.str();
 	//const char* sfs = (char*)sqlfmtStr.c_str();
-	string sql = "UPDATE t_pimpatent SET ZLH= ?, REASON=? WHERE PIMPATENTID = ?";
-	return sqlSession->executeUpdate(sql, "%s%s%s", uObj.getZLH(), uObj.getREASON(), uObj.getPIMPATENTID());
+
+	int CountReturn = 0;
+	string sql;
+	if (uObj.getZLH() != "string") {
+		sql = "UPDATE t_pimpatent SET ZLH= ? WHERE PIMPATENTID = ?";
+		CountReturn += sqlSession->executeUpdate(sql, "%s%s", uObj.getZLH(), uObj.getPIMPATENTID());
+	}
+	if (uObj.getUPDATEDATE() != "string") {
+		sql = "UPDATE t_pimpatent SET UPDATEDATE= ? WHERE PIMPATENTID = ?";
+		CountReturn += sqlSession->executeUpdate(sql, "%s%s", uObj.getUPDATEDATE(), uObj.getPIMPATENTID());
+	}
+
+	if (uObj.getZLHQSJ() != "string") {
+		sql = "UPDATE t_pimpatent SET ZLHQSJ= ? WHERE PIMPATENTID = ?";
+		CountReturn += sqlSession->executeUpdate(sql, "%s%s", uObj.getZLHQSJ(), uObj.getPIMPATENTID());
+	}
+
+	if (uObj.getPIMPATENTNAME() != "string") {
+		sql = "UPDATE t_pimpatent SET PIMPATENTNAME= ? WHERE PIMPATENTID = ?";
+		CountReturn += sqlSession->executeUpdate(sql, "%s%s", uObj.getPIMPATENTNAME(), uObj.getPIMPATENTID());
+	}
+
+	if (uObj.getUPDATEMAN() != "string") {
+		sql = "UPDATE t_pimpatent SET UPDATEMAN= ? WHERE PIMPATENTID = ?";
+		CountReturn += sqlSession->executeUpdate(sql, "%s%s", uObj.getUPDATEMAN(), uObj.getPIMPATENTID());
+	}
+
+	if (uObj.getCREATEMAN() != "string") {
+		sql = "UPDATE t_pimpatent SET CREATEMAN= ? WHERE PIMPATENTID = ?";
+		CountReturn += sqlSession->executeUpdate(sql, "%s%s", uObj.getCREATEMAN(), uObj.getPIMPATENTID());
+	}
+
+	if (uObj.getCREATEDATE() != "string") {
+		sql = "UPDATE t_pimpatent SET CREATEDATE= ? WHERE PIMPATENTID = ?";
+		CountReturn += sqlSession->executeUpdate(sql, "%s%s", uObj.getCREATEDATE(), uObj.getPIMPATENTID());
+	}
+
+	if (uObj.getZLPZGB() != "string") {
+		sql = "UPDATE t_pimpatent SET ZLPZGB= ? WHERE PIMPATENTID = ?";
+		CountReturn += sqlSession->executeUpdate(sql, "%s%s", uObj.getZLPZGB(), uObj.getPIMPATENTID());
+	}
+
+	if (uObj.getPIMPERSONID() != "string") {
+		sql = "UPDATE t_pimpatent SET PIMPERSONID= ? WHERE PIMPATENTID = ?";
+		CountReturn += sqlSession->executeUpdate(sql, "%s%s", uObj.getPIMPERSONID(), uObj.getPIMPATENTID());
+	}
+
+	if (uObj.getJLSS() != "string") {
+		sql = "UPDATE t_pimpatent SET JLSS= ? WHERE PIMPATENTID = ?";
+		CountReturn += sqlSession->executeUpdate(sql, "%s%s", uObj.getJLSS(), uObj.getPIMPATENTID());
+	}
+
+	if (uObj.getJLSPZT() != "string") {
+		sql = "UPDATE t_pimpatent SET JLSPZT= ? WHERE PIMPATENTID = ?";
+		CountReturn += sqlSession->executeUpdate(sql, "%s%s", uObj.getJLSPZT(), uObj.getPIMPATENTID());
+	}
+
+	if (uObj.getJLGLBH() != "string") {
+		sql = "UPDATE t_pimpatent SET JLGLBH= ? WHERE PIMPATENTID = ?";
+		CountReturn += sqlSession->executeUpdate(sql, "%s%s", uObj.getJLGLBH(), uObj.getPIMPATENTID());
+	}
+
+	if (uObj.getJLCZZ() != "string") {
+		sql = "UPDATE t_pimpatent SET JLCZZ= ? WHERE PIMPATENTID = ?";
+		CountReturn += sqlSession->executeUpdate(sql, "%s%s", uObj.getJLCZZ(), uObj.getPIMPATENTID());
+	}
+
+	if (uObj.getENCLOLURE() != "string") {
+		sql = "UPDATE t_pimpatent SET ENCLOLURE= ? WHERE PIMPATENTID = ?";
+		CountReturn += sqlSession->executeUpdate(sql, "%s%s", uObj.getENCLOLURE(), uObj.getPIMPATENTID());
+	}
+
+	if (uObj.getREASON() != "string") {
+		sql = "UPDATE t_pimpatent SET REASON=? WHERE PIMPATENTID = ?";
+		CountReturn += sqlSession->executeUpdate(sql, "%s%s", uObj.getREASON(), uObj.getPIMPATENTID());
+	}
+
+	if (CountReturn >= 1)
+		return 1;
+	else
+		return 0;
 }
 
