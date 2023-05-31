fb913732b5270329c2741ece5b8f46e7051150a5
gu-yier <3206266932@qq.com>
2023-05-31 11:50:36 +0800

    导出数据修改

--- hr-cpp/hr-c3-employeeinfo/controller/EmployeeInformation/EmployeeInformationController.cpp
+++ hr-cpp/hr-c3-employeeinfo/controller/EmployeeInformation/EmployeeInformationController.cpp
@@ -52,43 +52,50 @@ Uint64JsonVO::Wrapper EmployeeInformationController::execImportEmployeeInfo(cons
 //导出员工信息(导出本页在前端完成)
 EmployeeInformationPageJsonVO::Wrapper EmployeeInformationController::execExportEmployeeInfo(const EmployeeInformationPageQuery::Wrapper& exportInfo)
 {
-	// 定义一个JsonVO对象
-	auto vo = EmployeeInformationPageJsonVO::createShared();
-	// 定义一个分页对象
-	auto page = EmployeeInformationPageDTO::createShared();
-	page->pageIndex = exportInfo->pageIndex;
-	page->pageSize = exportInfo->pageSize;
-	page->total = 20;
-	page->calcPages();
-
-	// 模拟计算分页数据
-	int64_t start = (page->pageIndex.getValue(1) - 1) * page->pageSize.getValue(1);
-	int64_t end = start + page->pageSize.getValue(1);
-	// 边界值值处理
-	if (start >= page->total.getValue(0) || start < 0) {
-		vo->fail(page);
-		return vo;
-	}
-	if (end > page->total.getValue(0)) end = page->total.getValue(0);
-	// 循环生成测试数据
-	for (int64_t i = start; i < end; i++)
-	{
-		auto user = EmployeeInformationDTO::createShared();
-		user->name = exportInfo->name;
-		user->age = exportInfo->age;
-		user->id = exportInfo->id;
-		user->organize = exportInfo->organize;
-		user->depart = exportInfo->depart;
-		user->job = exportInfo->job;
-		user->post = exportInfo->post;
-		user->idMum = exportInfo->idMum;
-		user->birthday = exportInfo->birthday;
-		user->phone = exportInfo->phone;
-		user->state = exportInfo->state;
-		page->addData(user);
-	}
-	vo->success(page);
-	return vo;
+	//// 定义一个JsonVO对象
+	//auto vo = EmployeeInformationPageJsonVO::createShared();
+	//// 定义一个分页对象
+	//auto page = EmployeeInformationPageDTO::createShared();
+	//page->pageIndex = exportInfo->pageIndex;
+	//page->pageSize = exportInfo->pageSize;
+	//page->total = 20;
+	//page->calcPages();
+	//// 模拟计算分页数据
+	//int64_t start = (page->pageIndex.getValue(1) - 1) * page->pageSize.getValue(1);
+	//int64_t end = start + page->pageSize.getValue(1);
+	//// 边界值值处理
+	//if (start >= page->total.getValue(0) || start < 0) {
+	//	vo->fail(page);
+	//	return vo;
+	//}
+	//if (end > page->total.getValue(0)) end = page->total.getValue(0);
+	//// 循环生成测试数据
+	//for (int64_t i = start; i < end; i++)
+	//{
+	//	auto user = EmployeeInformationDTO::createShared();
+	//	user->name = exportInfo->name;
+	//	user->age = exportInfo->age;
+	//	user->id = exportInfo->id;
+	//	user->organize = exportInfo->organize;
+	//	user->depart = exportInfo->depart;
+	//	user->job = exportInfo->job;
+	//	user->post = exportInfo->post;
+	//	user->idMum = exportInfo->idMum;
+	//	user->birthday = exportInfo->birthday;
+	//	user->phone = exportInfo->phone;
+	//	user->state = exportInfo->state;
+	//	page->addData(user);
+	//}
+	//vo->success(page);
+	//return vo;
+	// 定义一个Service
+	EmployeeInformationServicer service;
+	// 查询数据
+	auto result = service.listAll(exportInfo);
+	// 响应结果
+	auto jvo = EmployeeInformationPageJsonVO::createShared();
+	jvo->success(result);
+	return jvo;
 
 }
 //新增员工信息 
