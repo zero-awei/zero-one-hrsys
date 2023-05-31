c1f7b11e6d835cad6504da24b4b1cec0794a7ba1
Andrew <1207368051@qq.com>
2023-05-31 12:51:27 +0800

    修改费用类型下拉列表service

--- hr-cpp/hr-c1-common/service/costTypeList/CostTypeListService.cpp
+++ hr-cpp/hr-c1-common/service/costTypeList/CostTypeListService.cpp
@@ -28,8 +28,9 @@ PullListDTO::Wrapper CostTypeListService::listAll()
 			dto->pullList->push_back(ItemDTO::createShared(atoi(code.c_str()), item.getCostType()));
 		}
 
-		// TODO: 将获取的数据更新到Redis缓存
-		UseLibRedis::updateRedis("cost-type", dao.getMapList());
+		if (res.size())
+			// TODO: 将获取的数据更新到Redis缓存
+			UseLibRedis::updateRedis("cost-type", dao.getMapList());
 	}
 	// 否则组装缓存数据到DTO
 	else
