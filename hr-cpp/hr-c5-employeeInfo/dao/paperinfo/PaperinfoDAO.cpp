cae80ed2e3ed3ea556592f60039bf2c16a470e90
jiang <yslonn@qq.com>
2023-06-01 00:30:46 +0800

    jiang：查询dao将like语句改为“=”，修改dao仅修改页面字段

--- hr-cpp/hr-c5-employeeInfo/dao/paperinfo/PaperinfoDAO.cpp
+++ hr-cpp/hr-c5-employeeInfo/dao/paperinfo/PaperinfoDAO.cpp
@@ -28,7 +28,7 @@ std::list<t_pimpaperDO> PaperinfoDAO::selectBypimpaperid(const string& st)
 		KWMC,\
 		KWQS,\
 		REASON\
-		FROM t_pimpaper WHERE 1 = 1 AND PIMPAPERID LIKE CONCAT('%', ? , '%')";
+		FROM t_pimpaper WHERE 1 = 1 AND PIMPAPERID = ? ";
 	PatentinfoMapper mapper;
 	return sqlSession->executeQuery<t_pimpaperDO, PaperinfoMapper>(sql, mapper, "%s", st);
 }
@@ -37,121 +37,17 @@ std::list<t_pimpaperDO> PaperinfoDAO::selectBypimpaperid(const string& st)
 // 修改数据
 int PaperinfoDAO::update(const t_pimpaperDO& uObj)
 {
-	int CountReturn = 0;//确定是否返回正确
 	int CountMark = 0;//标识修改语句使“，”位置正确
 	stringstream sql;
 	sql << "UPDATE t_pimpaper SET ";
 	SqlParams params;
-	if (uObj.getGRZLWZZZDPM() != "string") {
-		if (CountMark != 0) {
-			sql << ",";
-		}
-		sql << " GRZLWZZZDPM= ? ";
-		SQLPARAMS_PUSH(params, "s", std::string, uObj.getGRZLWZZZDPM());
-		CountMark++;
-	}
-	if (uObj.getUPDATEMAN() != "string") {
-		if (CountMark != 0) {
-			sql << ",";
-		}
-		sql << " UPDATEMAN= ? ";
-		SQLPARAMS_PUSH(params, "s", std::string, uObj.getUPDATEMAN());
-		CountMark++;
-	}
-	if (uObj.getCREATEDATE() != "string") {
-		if (CountMark != 0) {
-			sql << ",";
-		}
-		sql << " CREATEDATE= ? ";
-		SQLPARAMS_PUSH(params, "s", std::string, uObj.getCREATEDATE());
-		CountMark++;
-	}
-	if (uObj.getUPDATEDATE() != "string") {
-		if (CountMark != 0) {
-			sql << ",";
-		}
-		sql << " UPDATEDATE= ? ";
-		SQLPARAMS_PUSH(params, "s", std::string, uObj.getUPDATEDATE());
-		CountMark++;
-	}
-	if (uObj.getCREATEMAN() != "string") {
-		if (CountMark != 0) {
-			sql << ",";
-		}
-		sql << " CREATEMAN= ? ";
-		SQLPARAMS_PUSH(params, "s", std::string, uObj.getCREATEMAN());
-		CountMark++;
-	}
-	if (uObj.getFBSJ() != "string") {
-		if (CountMark != 0) {
-			sql << ",";
-		}
-		sql << " FBSJ= ? ";
-		SQLPARAMS_PUSH(params, "s", std::string, uObj.getFBSJ());
-		CountMark++;
-	}
+
 	if (uObj.getPIMPAPERNAME() != "string") {
 		if (CountMark != 0) {
 			sql << ",";
 		}
 		sql << " PIMPAPERNAME= ? ";
 		SQLPARAMS_PUSH(params, "s", std::string, uObj.getPIMPAPERNAME());
-		CountMark++;
-	}
-	if (uObj.getCBSHKWMC() != "string") {
-		if (CountMark != 0) {
-			sql << ",";
-		}
-		sql << " CBSHKWMC= ? ";
-		SQLPARAMS_PUSH(params, "s", std::string, uObj.getCBSHKWMC());
-		CountMark++;
-	}
-	if (uObj.getPIMPERSONID() != "string") {
-		if (CountMark != 0) {
-			sql << ",";
-		}
-		sql << " PIMPERSONID= ? ";
-		SQLPARAMS_PUSH(params, "s", std::string, uObj.getPIMPERSONID());
-		CountMark++;
-	}
-	if (uObj.getFJ() != "string") {
-		if (CountMark != 0) {
-			sql << ",";
-		}
-		sql << " FJ= ? ";
-		SQLPARAMS_PUSH(params, "s", std::string, uObj.getFJ());
-		CountMark++;
-	}
-	if (uObj.getJLSPZT() != "string") {
-		if (CountMark != 0) {
-			sql << ",";
-		}
-		sql << " JLSPZT= ? ";
-		SQLPARAMS_PUSH(params, "s", std::string, uObj.getJLSPZT());
-		CountMark++;
-	}
-	if (uObj.getJLGLBH() != "string") {
-		if (CountMark != 0) {
-			sql << ",";
-		}
-		sql << " JLGLBH= ? ";
-		SQLPARAMS_PUSH(params, "s", std::string, uObj.getJLGLBH());
-		CountMark++;
-	}
-	if (uObj.getJLCZZ() != "string") {
-		if (CountMark != 0) {
-			sql << ",";
-		}
-		sql << " JLCZZ= ? ";
-		SQLPARAMS_PUSH(params, "s", std::string, uObj.getJLCZZ());
-		CountMark++;
-	}
-	if (uObj.getJLSS() != "string") {
-		if (CountMark != 0) {
-			sql << ",";
-		}
-		sql << " JLSS= ? ";
-		SQLPARAMS_PUSH(params, "s", std::string, uObj.getJLSS());
 		CountMark++;
 	}
 	if (uObj.getCBS() != "string") {
@@ -178,12 +74,28 @@ int PaperinfoDAO::update(const t_pimpaperDO& uObj)
 		SQLPARAMS_PUSH(params, "s", std::string, uObj.getKWQS());
 		CountMark++;
 	}
-	if (uObj.getREASON() != "string") {
+	if (uObj.getFBSJ() != "string") {
 		if (CountMark != 0) {
 			sql << ",";
 		}
-		sql << " REASON= ? ";
-		SQLPARAMS_PUSH(params, "s", std::string, uObj.getREASON());
+		sql << " FBSJ= ? ";
+		SQLPARAMS_PUSH(params, "s", std::string, uObj.getFBSJ());
+		CountMark++;
+	}
+	if (uObj.getGRZLWZZZDPM() != "string") {
+		if (CountMark != 0) {
+			sql << ",";
+		}
+		sql << " GRZLWZZZDPM= ? ";
+		SQLPARAMS_PUSH(params, "s", std::string, uObj.getGRZLWZZZDPM());
+		CountMark++;
+	}
+	if (uObj.getFJ() != "string") {
+		if (CountMark != 0) {
+			sql << ",";
+		}
+		sql << " FJ= ? ";
+		SQLPARAMS_PUSH(params, "s", std::string, uObj.getFJ());
 		CountMark++;
 	}
 
@@ -192,11 +104,6 @@ int PaperinfoDAO::update(const t_pimpaperDO& uObj)
 		SQLPARAMS_PUSH(params, "s", std::string, uObj.getPIMPAPERID());
 	}
 	string sqlStr = sql.str();
-	CountReturn += sqlSession->executeUpdate(sqlStr, params);
-
-	if (CountReturn != 0)
-		return 1;
-	else
-		return 0;
+	return sqlSession->executeUpdate(sqlStr, params);
 }
 
--- hr-cpp/hr-c5-employeeInfo/dao/patentinfo/PatentinfoDAO.cpp
+++ hr-cpp/hr-c5-employeeInfo/dao/patentinfo/PatentinfoDAO.cpp
@@ -13,7 +13,7 @@ std::list<PatentinfoDO> PatentinfoDAO::selectByPIMPATENTID(const string& pimpate
 					UPDATEMAN,CREATEMAN,CREATEDATE,ZLPZGB,PIMPERSONID,\
 					JLSS,JLSPZT,JLGLBH,JLCZZ,ENCLOLURE,\
 					REASON\
-		FROM t_pimpatent WHERE 1 = 1 AND PIMPATENTID LIKE CONCAT('%', ?, '%')";
+		FROM t_pimpatent WHERE 1 = 1 AND PIMPATENTID = ? ";
 	PatentinfoMapper mapper;
 	return sqlSession->executeQuery<PatentinfoDO, PatentinfoMapper>(sql, mapper, "%s", pimpatentid);
 }
@@ -22,7 +22,142 @@ std::list<PatentinfoDO> PatentinfoDAO::selectByPIMPATENTID(const string& pimpate
 // 修改数据
 int PatentinfoDAO::update(const PatentinfoDO& uObj)
 {
-	int CountReturn = 0;//确定是否返回正确
+	//int CountReturn = 0;//确定是否返回正确
+	//int CountMark = 0;//标识修改语句使“，”位置正确
+	//stringstream sql;
+	//sql << "UPDATE t_pimpatent SET ";
+	//SqlParams params; 
+	//if (uObj.getZLH() != "string") {
+	//	if (CountMark != 0) {
+	//		sql << ",";
+	//	}
+	//	sql << " ZLH= ? ";
+	//	SQLPARAMS_PUSH(params, "s", std::string, uObj.getZLH()); 
+	//	CountMark++;
+	//}
+	//if (uObj.getUPDATEDATE() != "string") {
+	//	if (CountMark != 0) {
+	//		sql << ",";
+	//	}
+	//	sql << " UPDATEDATE= ? ";
+	//	SQLPARAMS_PUSH(params, "s", std::string, uObj.getUPDATEDATE());
+	//	CountMark++;
+	//}
+	//if (uObj.getZLHQSJ() != "string") {
+	//	if (CountMark != 0) {
+	//		sql << ",";
+	//	}
+	//	sql << " ZLHQSJ= ? ";
+	//	SQLPARAMS_PUSH(params, "s", std::string, uObj.getZLHQSJ());
+	//	CountMark++;
+	//}
+	//if (uObj.getPIMPATENTNAME() != "string") {
+	//	if (CountMark != 0) {
+	//		sql << ",";
+	//	}
+	//	sql << " PIMPATENTNAME= ? ";
+	//	SQLPARAMS_PUSH(params, "s", std::string, uObj.getPIMPATENTNAME());
+	//	CountMark++;
+	//}
+	//if (uObj.getUPDATEMAN() != "string") {
+	//	if (CountMark != 0) {
+	//		sql << ",";
+	//	}
+	//	sql << " UPDATEMAN= ? ";
+	//	SQLPARAMS_PUSH(params, "s", std::string, uObj.getUPDATEMAN());
+	//	CountMark++;
+	//}
+	//if (uObj.getCREATEMAN() != "string") {
+	//	if (CountMark != 0) {
+	//		sql << ",";
+	//	}
+	//	sql << " CREATEMAN= ? ";
+	//	SQLPARAMS_PUSH(params, "s", std::string, uObj.getCREATEMAN());
+	//	CountMark++;
+	//}
+	//if (uObj.getCREATEDATE() != "string") {
+	//	if (CountMark != 0) {
+	//		sql << ",";
+	//	}
+	//	sql << " CREATEDATE= ? ";
+	//	SQLPARAMS_PUSH(params, "s", std::string, uObj.getCREATEDATE());
+	//	CountMark++;
+	//}
+	//if (uObj.getZLPZGB() != "string") {
+	//	if (CountMark != 0) {
+	//		sql << ",";
+	//	}
+	//	sql << " ZLPZGB= ? ";
+	//	SQLPARAMS_PUSH(params, "s", std::string, uObj.getZLPZGB());
+	//	CountMark++;
+	//}
+	//if (uObj.getPIMPERSONID() != "string") {
+	//	if (CountMark != 0) {
+	//		sql << ",";
+	//	}
+	//	sql << " PIMPERSONID= ? ";
+	//	SQLPARAMS_PUSH(params, "s", std::string, uObj.getPIMPERSONID());
+	//	CountMark++;
+	//}
+	//if (uObj.getJLSS() != "string") {
+	//	if (CountMark != 0) {
+	//		sql << ",";
+	//	}
+	//	sql << " JLSS= ? ";
+	//	SQLPARAMS_PUSH(params, "s", std::string, uObj.getJLSS());
+	//	CountMark++;
+	//}
+	//if (uObj.getJLSPZT() != "string") {
+	//	if (CountMark != 0) {
+	//		sql << ",";
+	//	}
+	//	sql << " JLSPZT= ? ";
+	//	SQLPARAMS_PUSH(params, "s", std::string, uObj.getJLSPZT());
+	//	CountMark++;
+	//}
+	//if (uObj.getJLGLBH() != "string") {
+	//	if (CountMark != 0) {
+	//		sql << ",";
+	//	}
+	//	sql << " JLGLBH= ? ";
+	//	SQLPARAMS_PUSH(params, "s", std::string, uObj.getJLGLBH());
+	//	CountMark++;
+	//}
+	//if (uObj.getJLCZZ() != "string") {
+	//	if (CountMark != 0) {
+	//		sql << ",";
+	//	}
+	//	sql << " JLCZZ= ? ";
+	//	SQLPARAMS_PUSH(params, "s", std::string, uObj.getJLCZZ());
+	//	CountMark++;
+	//}
+	//if (uObj.getENCLOLURE() != "string") {
+	//	if (CountMark != 0) {
+	//		sql << ",";
+	//	}
+	//	sql << " ENCLOLURE= ? ";
+	//	SQLPARAMS_PUSH(params, "s", std::string, uObj.getENCLOLURE());
+	//	CountMark++;
+	//}
+	//if (uObj.getREASON() != "string") {
+	//	if (CountMark != 0) {
+	//		sql << ",";
+	//	}
+	//	sql << " REASON= ? ";
+	//	SQLPARAMS_PUSH(params, "s", std::string, uObj.getREASON());
+	//	CountMark++;
+	//}
+	//if (uObj.getPIMPATENTID() != "string") {
+	//	sql << "WHERE PIMPATENTID = ?";
+	//	SQLPARAMS_PUSH(params, "s", std::string, uObj.getPIMPATENTID());
+	//}
+	//string sqlStr = sql.str();
+	//CountReturn += sqlSession->executeUpdate(sqlStr, params);
+	//if (CountReturn != 0)
+	//	return 1;
+	//else
+	//	return 0;
+
 	int CountMark = 0;//标识修改语句使“，”位置正确
 	stringstream sql;
 	sql << "UPDATE t_pimpatent SET ";
@@ -32,28 +167,9 @@ int PatentinfoDAO::update(const PatentinfoDO& uObj)
 			sql << ",";
 		}
 		sql << " ZLH= ? ";
-		SQLPARAMS_PUSH(params, "s", std::string, uObj.getZLH()); 
+		SQLPARAMS_PUSH(params, "s", std::string, uObj.getZLH());
 		CountMark++;
 	}
-
-	if (uObj.getUPDATEDATE() != "string") {
-		if (CountMark != 0) {
-			sql << ",";
-		}
-		sql << " UPDATEDATE= ? ";
-		SQLPARAMS_PUSH(params, "s", std::string, uObj.getUPDATEDATE());
-		CountMark++;
-	}
-
-	if (uObj.getZLHQSJ() != "string") {
-		if (CountMark != 0) {
-			sql << ",";
-		}
-		sql << " ZLHQSJ= ? ";
-		SQLPARAMS_PUSH(params, "s", std::string, uObj.getZLHQSJ());
-		CountMark++;
-	}
-
 	if (uObj.getPIMPATENTNAME() != "string") {
 		if (CountMark != 0) {
 			sql << ",";
@@ -62,34 +178,14 @@ int PatentinfoDAO::update(const PatentinfoDO& uObj)
 		SQLPARAMS_PUSH(params, "s", std::string, uObj.getPIMPATENTNAME());
 		CountMark++;
 	}
-
-	if (uObj.getUPDATEMAN() != "string") {
+	if (uObj.getZLHQSJ() != "string") {
 		if (CountMark != 0) {
 			sql << ",";
 		}
-		sql << " UPDATEMAN= ? ";
-		SQLPARAMS_PUSH(params, "s", std::string, uObj.getUPDATEMAN());
+		sql << " ZLHQSJ= ? ";
+		SQLPARAMS_PUSH(params, "s", std::string, uObj.getZLHQSJ());
 		CountMark++;
 	}
-
-	if (uObj.getCREATEMAN() != "string") {
-		if (CountMark != 0) {
-			sql << ",";
-		}
-		sql << " CREATEMAN= ? ";
-		SQLPARAMS_PUSH(params, "s", std::string, uObj.getCREATEMAN());
-		CountMark++;
-	}
-
-	if (uObj.getCREATEDATE() != "string") {
-		if (CountMark != 0) {
-			sql << ",";
-		}
-		sql << " CREATEDATE= ? ";
-		SQLPARAMS_PUSH(params, "s", std::string, uObj.getCREATEDATE());
-		CountMark++;
-	}
-
 	if (uObj.getZLPZGB() != "string") {
 		if (CountMark != 0) {
 			sql << ",";
@@ -98,52 +194,6 @@ int PatentinfoDAO::update(const PatentinfoDO& uObj)
 		SQLPARAMS_PUSH(params, "s", std::string, uObj.getZLPZGB());
 		CountMark++;
 	}
-
-	if (uObj.getPIMPERSONID() != "string") {
-		if (CountMark != 0) {
-			sql << ",";
-		}
-		sql << " PIMPERSONID= ? ";
-		SQLPARAMS_PUSH(params, "s", std::string, uObj.getPIMPERSONID());
-		CountMark++;
-	}
-
-	if (uObj.getJLSS() != "string") {
-		if (CountMark != 0) {
-			sql << ",";
-		}
-		sql << " JLSS= ? ";
-		SQLPARAMS_PUSH(params, "s", std::string, uObj.getJLSS());
-		CountMark++;
-	}
-
-	if (uObj.getJLSPZT() != "string") {
-		if (CountMark != 0) {
-			sql << ",";
-		}
-		sql << " JLSPZT= ? ";
-		SQLPARAMS_PUSH(params, "s", std::string, uObj.getJLSPZT());
-		CountMark++;
-	}
-
-	if (uObj.getJLGLBH() != "string") {
-		if (CountMark != 0) {
-			sql << ",";
-		}
-		sql << " JLGLBH= ? ";
-		SQLPARAMS_PUSH(params, "s", std::string, uObj.getJLGLBH());
-		CountMark++;
-	}
-
-	if (uObj.getJLCZZ() != "string") {
-		if (CountMark != 0) {
-			sql << ",";
-		}
-		sql << " JLCZZ= ? ";
-		SQLPARAMS_PUSH(params, "s", std::string, uObj.getJLCZZ());
-		CountMark++;
-	}
-
 	if (uObj.getENCLOLURE() != "string") {
 		if (CountMark != 0) {
 			sql << ",";
@@ -152,26 +202,16 @@ int PatentinfoDAO::update(const PatentinfoDO& uObj)
 		SQLPARAMS_PUSH(params, "s", std::string, uObj.getENCLOLURE());
 		CountMark++;
 	}
-
-	if (uObj.getREASON() != "string") {
-		if (CountMark != 0) {
-			sql << ",";
-		}
-		sql << " REASON= ? ";
-		SQLPARAMS_PUSH(params, "s", std::string, uObj.getREASON());
-		CountMark++;
-	}
-
 	if (uObj.getPIMPATENTID() != "string") {
 		sql << "WHERE PIMPATENTID = ?";
 		SQLPARAMS_PUSH(params, "s", std::string, uObj.getPIMPATENTID());
 	}
 	string sqlStr = sql.str();
-	CountReturn += sqlSession->executeUpdate(sqlStr, params);
+	return sqlSession->executeUpdate(sqlStr, params);
 
-	if (CountReturn != 0)
-		return 1;
-	else
-		return 0;
+
+	//string sql = "UPDATE `t_pimpatent` SET `ZLH`=?, `PIMPATENTNAME`=?, `ZLHQSJ`=? ,`ZLPZGB`=? ,`ENCLOLURE`=?  WHERE `PIMPATENTID`=?";
+	//return sqlSession->executeUpdate(sql, "%s%s%s%s%s%s", uObj.getZLH(), uObj.getPIMPATENTNAME(), uObj.getZLHQSJ(), uObj.getZLPZGB(), uObj.getENCLOLURE(), uObj.getPIMPATENTID());
+
 }
 
