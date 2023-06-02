c2cb2b4a66a712f1beb7cd9c8d07c6e67de1af33
微微 <15272516991@163.com>
2023-06-02 20:35:49 +0800

    补全示例文件

---
+++ hr-frontend/src/views/sample/communication/Index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hr-btns">
+    <A></A>
+    <B></B>
+  </div>
+</template>
+
+<script setup>
+import A from './A.vue'
+import B from './B.vue'
+
+</script>
+
+<style lang="scss" scoped>
+@include block(btns) {
+  width: 250px;
+  display: flex;
+  justify-content: space-between;
+  border: 2px solid #ccc;
+}
+</style>
