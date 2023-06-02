87d44510b57d6a34f3b0ac47d1416f9ad940b6a0
sayoriqwq <2531600563@qq.com>
2023-06-02 23:24:08 +0800

    系统管理页面

---
+++ hr-frontend/src/views/sysManage/RolesManage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <TableHead
+      :tableTitle="$store.title"
+      :addTitle="$store.title"
+      :tableOperations="$store.options"
+      :dataitem="$store.dataitem"
+      :saveData="$store.addData"
+    >
+    </TableHead>
+    <MainTable
+      :xml-data="$store.xmlData"
+      :table-data="$store.tableData"
+    ></MainTable>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import TableHead from '@/components/table/head/TableHead.vue'
+import MainTable from '@/components/MainTable.vue'
+import { onBeforeMount } from 'vue'
+import { roleManageStore } from '@/stores/roleManage'
+
+const $store = roleManageStore()
+onBeforeMount(() => {
+  $store.initTableData()
+  $store.initDataItem()
+})
+</script>
+
+<style></style>
