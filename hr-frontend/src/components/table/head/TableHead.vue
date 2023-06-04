8eb22f0a7b33918c08b97e2fb0c991fd9011b122
微微 <15272516991@163.com>
2023-06-04 17:22:05 +0800

    合并表头组件修改部分

--- hr-frontend/src/components/table/head/TableHead.vue
+++ hr-frontend/src/components/table/head/TableHead.vue
@@ -1,43 +1,86 @@
 <template>
-    <div class="hr-table-header">
-        <div class="hr-table-header__title">{{tableTitle}}</div>
-        <div class="hr-table-header__menus">
-            <el-row class="row-bg" justify="end">
-                  <div v-for="tableOperation in tableOperations" :key="tableOperation.name">
-                    <SearchBox v-if="tableOperation.name==='搜索'"/>
-                    <div style="postion:relative;top:4px">
-                      <AddButton :tableTitle="tableTitle" :addTitle="addTitle" :dataitem="dataitem" :saveData="saveData" v-if="tableOperation.name==='新增'"/>
-                    </div>
-                  </div>
-            </el-row>            
+  <div class="hr-table-header">
+    <div class="hr-table-header__title">{{ tableTitle }}</div>
+    <div class="hr-table-header__menus">
+      <el-row class="row-bg" justify="end">
+        <div
+          v-for="tableOperation in tableOperations"
+          :key="tableOperation.name"
+        >
+          <SearchBox v-if="tableOperation.name === '搜索'" />
+          <div style="postion: relative; top: 4px">
+            <AddButton
+              :tableTitle="tableTitle"
+              :addTitle="addTitle"
+              :dataitem="dataitem"
+              :saveData="saveData"
+              v-if="tableOperation.name === '新增'"
+            />
+            <el-button plain v-if="tableOperation.name === '删除'">
+              <span class="hr-button">
+                <IconDocument class="hr-button__icon" />
+                <p class="hr-button__p">删除</p>
+              </span>
+            </el-button>
+          </div>
         </div>
+      </el-row>
     </div>
+  </div>
 </template>
 
 <script setup>
-import AddButton from "./AddButton.vue"
-import SearchBox from "@/components/SearchBox.vue"
+import AddButton from './AddButton.vue'
+import SearchBox from '@/components/SearchBox.vue'
 
-defineProps(['tableTitle','addTitle','tableOperations','dataitem','saveData'])
+defineProps([
+  'tableTitle',
+  'addTitle',
+  'tableOperations',
+  'dataitem',
+  'saveData'
+])
 </script>
 
 <style lang="scss" scoped>
-
-.hr-table-header{
-    width: 100%;
-    height: 40px;
-    padding: 0 10px 10px 10px;
-    @include element(title){
-        float: left;
-        width: 120px;
-        height: 32px;
-        font-size: 16px;
-        font-weight: 600;
-    }
-    // @include element(menus){
-    //     float: right;
-    //     height: 100%;
-    // }
+.hr-table-header {
+  width: 100%;
+  height: 40px;
+  padding: 0 10px 10px 10px;
+  @include element(title) {
+    float: left;
+    width: 120px;
+    height: 32px;
+    font-size: 16px;
+    font-weight: 600;
+  }
+  // @include element(menus){
+  //     float: right;
+  //     height: 100%;
+  // }
+}
+.hr-button{
+  display:flex;
+  @include element(icon){
+    width: 14px;
+    height: 16px;
+    margin-right: 6px;
+  }
+  @include element(p){
+    font-size: 14px;
+    margin-top: 1.5px;
+  }
+}
+.el-button{
+  width: 60px;
+  height: 32px;
+  padding: 0 6px;
+  border: 0;
+}
+.el-button--text {
+  width: 64px;
+  height: 32px;
+  margin-right: 15px;
 }
 .el-row {
   margin-bottom: 20px;
