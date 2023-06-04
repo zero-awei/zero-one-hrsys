07f24eacb9213dbbe22342bdc5cbd0da3bd9bf29
nkunick <1020296070@qq.com>
2023-06-04 23:53:25 +0800

    提交奖惩信息vo

---
+++ hr-cpp/hr-c4-basemgeminfo/domain/vo/rewardandpunishmentinformation/RewardAndPunishmentInformationPageQuery.h
@@ -0,0 +1,61 @@
+#pragma once
+/*
+ Copyright Zero One Star. All rights reserved.
+
+ @Author: arui
+ @Date: 2023/05/24 19:47:39
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+	  https://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+#ifndef _REWARDANDPUNISHMENTINFORMATIONPAGEQUERY_H_
+#define _REWARDANDPUNISHMENTINFORMATIONPAGEQUERY_H_
+
+#include "../../GlobalInclude.h"
+#include "domain/query/PageQuery.h"
+#include OATPP_CODEGEN_BEGIN(DTO)
+
+class RewardAndPunishmentInformationPageQuery : public PageQuery
+{
+	DTO_INIT(RewardAndPunishmentInformationPageQuery, PageQuery);
+	
+	// 员工编号
+	DTO_FIELD(String, pimpersonid);
+	DTO_FIELD_INFO(pimpersonid) {
+		info->description = ZH_WORDS_GETTER("rewardandpunishmentinformation.field.pimpersionid");
+	}
+};
+class RewardAndPunishmentInformationViewQuery : public oatpp::DTO
+{
+	DTO_INIT(RewardAndPunishmentInformationViewQuery, DTO);
+
+	// 奖惩信息标识(主键)
+	DTO_FIELD(String, pimrewardpunishmentid);
+	DTO_FIELD_INFO(pimrewardpunishmentid) {
+		info->description = ZH_WORDS_GETTER("rewardandpunishmentinformation.field.pimrewardpunishmentid");
+	}
+};
+class RewardAndPunishmentInformationDownloadQuery : public oatpp::DTO
+{
+	DTO_INIT(RewardAndPunishmentInformationDownloadQuery, DTO);
+
+	// 科研成果人员信息标识
+	DTO_FIELD(String, pimpersonid);
+	DTO_FIELD_INFO(pimpersonid) {
+		info->description = ZH_WORDS_GETTER("rewardandpunishmentinformation.field.pimpersonid");
+	}
+};
+
+
+#include OATPP_CODEGEN_END(DTO)
+
+#endif // !_REWARDANDPUNISHMENTINFORMATIONPAGEQUERY_H_
