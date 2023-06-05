e8d6fc1c1a40467b29a1c7cffb02001da390b11d
hzs0922 <15759664149@163.com>
2023-06-05 21:59:29 +0800

    补充注释

--- hr-cpp/hr-c1-orgmanage/controller/projTag/pageQueryProjTagList/PageQueryProjTagListController.h
+++ hr-cpp/hr-c1-orgmanage/controller/projTag/pageQueryProjTagList/PageQueryProjTagListController.h
@@ -1,4 +1,22 @@
 #pragma once
+/*
+ Copyright Zero One Star. All rights reserved.
+
+ @Author: 咫尺之书
+ @Date: 2023/05/24 17:58:45
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+	  https://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
 #ifndef _PAGEQUERYPROJTAGLISTCONTROELLER_H_
 #define _PAGEQUERYPROJTAGLISTCONTROELLER_H_
 
@@ -10,7 +28,11 @@
 #include "domain/vo/projTag/PageQueryProjTagVO.h"
 
 #include OATPP_CODEGEN_BEGIN(ApiController)
-
+/**
+ * 分页查询项目标签列表控制器
+ * 负责人：咫尺之书
+ * 对应数据表：t_ormxmbq
+ */
 class PageQueryProjTagListController : public oatpp::web::server::api::ApiController
 {
 	// 定义控制器访问入口
--- hr-cpp/hr-c1-orgmanage/domain/query/projTag/PageProjTagQuery.h
+++ hr-cpp/hr-c1-orgmanage/domain/query/projTag/PageProjTagQuery.h
@@ -1,4 +1,22 @@
 #pragma once
+/*
+ Copyright Zero One Star. All rights reserved.
+
+ @Author: 咫尺之书
+ @Date: 2023/05/24 18:22:04
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+	  https://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
 #ifndef _PAGEPROJTAGQUERY_H_
 #define _PAGEPROJTAGQUERY_H_
 
@@ -14,10 +32,12 @@
 class PageProjTagQuery : public PageQuery
 {
 	DTO_INIT(PageProjTagQuery, PageQuery);
+	// 项目标签名
 	DTO_FIELD_INFO(tagName) {
 		info->description = ZH_WORDS_GETTER("projTag.pageQuery.tagName");
 	}
 	DTO_FIELD(String, tagName);
+	// 查询排序
 	DTO_FIELD_INFO(order) {
 		info->description = ZH_WORDS_GETTER("projTag.pageQuery.order");
 	}
