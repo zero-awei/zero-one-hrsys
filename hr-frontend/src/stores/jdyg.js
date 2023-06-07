6a12ce8a4dcaaee3752e81ff849431cf02039f84
sayoriqwq <2531600563@qq.com>
2023-06-07 10:24:45 +0800

    迁移到store

---
+++ hr-frontend/src/stores/jdyg.js
@@ -0,0 +1,154 @@
+import { defineStore } from 'pinia'
+
+export const RentStaffStore = defineStore('jdyg', {
+  state: () => ({
+    title: '借调人员花名册',
+    options: [{ name: '搜索' }, { name: '导出' }, { name: '过滤' }],
+    tableData: null,
+    dataitem: null,
+    xmlData: [
+      { id: 1, name: '员工编号', prop: 'id' },
+      { id: 2, name: '员工姓名', prop: 'name' },
+      { id: 3, name: '原组织', prop: 'originalOrganization' },
+      { id: 4, name: '原部门', prop: 'originalDepartment' },
+      { id: 5, name: '原职务', prop: 'originalPosition' },
+      { id: 6, name: '原岗位', prop: 'originalJob' },
+      { id: 7, name: '新组织', prop: 'newOrganization' },
+      { id: 8, name: '新部门', prop: 'newDepartment' },
+      { id: 9, name: '新职务', prop: 'newPosition' },
+      { id: 10, name: '新岗位', prop: 'newJob' },
+      { id: 11, name: '借调状态', prop: 'rentState' },
+      { id: 12, name: '借调开始时间', prop: 'rentStartTime' },
+      { id: 13, name: '借调结束时间', prop: 'rentEndTime' }
+    ],
+
+    //分页器
+    pageIndex: 1,
+    pageSize: 100,
+    total: 100
+    // pageSizes:[]
+  }),
+  actions: {
+    initTableData() {
+      this.tableData = [
+        {
+          id: 10001,
+          name: '彭于晏',
+          originalOrganization: '山东公司',
+          originalDepartment: '人力资源部',
+          originalPosition: '助理总经理',
+          originalJob: '工会主席',
+          newOrganization: 'xxx',
+          newDepartment: '市场部',
+          newPosition: '部门经理',
+          newJob: '薪资专员',
+          rentState: '借调中',
+          rentStartTime: '2023-05-24',
+          rentEndTime: '2023-05-26'
+        },
+        {
+          id: 10002,
+          name: '彭于',
+          originalOrganization: '山东公司',
+          originalDepartment: '人力资源部',
+          originalPosition: '助理总经理',
+          originalJob: '工会主席',
+          newOrganization: 'xxx',
+          newDepartment: '市场部',
+          newPosition: '部门经理',
+          newJob: '薪资专员',
+          rentState: '借调中',
+          rentStartTime: '2023-05-24',
+          rentEndTime: '2023-05-26'
+        },
+        {
+          id: 10004,
+          name: '彭啥的',
+          originalOrganization: '山东公司',
+          originalDepartment: '人力资源部',
+          originalPosition: '助理总经理',
+          originalJob: '工会主席',
+          newOrganization: 'xxx',
+          newDepartment: '市场部',
+          newPosition: '部门经理',
+          newJob: '薪资专员',
+          rentState: '借调中',
+          rentStartTime: '2023-05-24',
+          rentEndTime: '2023-05-26'
+        }
+      ]
+    },
+    initDataItem() {
+      this.dataitem = [
+        {
+          label: '员工编号',
+          name: 'id',
+          type: Number
+        },
+        {
+          label: '员工姓名',
+          name: 'name',
+          type: String
+        },
+        {
+          label: '原组织',
+          name: 'originalOrganization',
+          type: String
+        },
+        {
+          label: '原部门',
+          name: 'originalDepartment',
+          type: String
+        },
+        {
+          label: '原职务',
+          name: 'originalPosition',
+          type: String
+        },
+        {
+          label: '原岗位',
+          name: 'originalJob',
+          type: String
+        },
+        {
+          label: '新组织',
+          name: 'newOrganization',
+          type: String
+        },
+        {
+          label: '新部门',
+          name: 'newDepartment',
+          type: String
+        },
+        {
+          label: '新职务',
+          name: 'newPosition',
+          type: String
+        },
+        {
+          label: '新岗位',
+          name: 'newJob',
+          type: String
+        },
+        {
+          label: '借调状态',
+          name: 'rentState',
+          type: String
+        },
+        {
+          label: '借调开始时间',
+          name: 'rentStartTime',
+          type: String
+        },
+        {
+          label: '借调结束时间',
+          name: 'rentEndTime',
+          type: String
+        }
+      ]
+    },
+    addData(val) {
+      this.tableData.push(val)
+    }
+  }
+})
--- hr-frontend/src/views/baseManage/roster/Syqyg.vue
+++ hr-frontend/src/views/baseManage/roster/Syqyg.vue
@@ -1,8 +1,21 @@
 <template>
   <div>
-    <TableHead :tableTitle="title" :tableOperations="options"> </TableHead>
-    <MainTable :xml-data="xmlData" :table-data="tableData"></MainTable>
-    <Pagination :pageSizes="pagesizes" :total="pagenumber" />
+    <div class="header">
+      <TableHead
+        :tableTitle="$store.title"
+        :tableOperations="$store.options"
+        :saveData="$store.addData"
+        :addTitle="$store.title"
+        :dataitem="$store.dataitem"
+      />
+    </div>
+    <div class="table">
+      <MainTable :xmlData="newXmlData" :tableData="$store.tableData" />
+    </div>
+    <div class="footer">
+      <ColumnFilter :xmlData="$store.xmlData" :parentMethod="getNewXmlData" />
+      <Pagination :currentPage="$store.pageIndex" :pageSize="$store.pageSize" />
+    </div>
   </div>
 </template>
 
@@ -10,111 +23,28 @@
 import TableHead from '@/components/table/head/TableHead.vue'
 import Pagination from '@/components/pagination/Pagination.vue'
 import MainTable from '@/components/MainTable.vue'
-import { ref, reactive } from 'vue'
-const title = ref('试用员工')
-const options = reactive([
-  {
-    name: '搜索'
-  },
-  {
-    name: '导出'
-  }
-])
-interface User {
-  //自定义数据
-  id: number
-  name: string
-  organization: string
-  department: string
-  position: string
-  job: string
-  //时间我用的string
-  comeTime: string
-  expirTime: string
+import { onBeforeMount, ref } from 'vue'
+import { ProbationaryStaffStore } from '@/stores/syqyg'
+
+const $store = ProbationaryStaffStore()
+
+onBeforeMount(() => {
+  $store.initTableData()
+  $store.initDataItem()
+})
+function getNewXmlData(checkStatus) {
+  newXmlData.value = $store.xmlData.filter((item) => {
+    return checkStatus.value.includes(item.name)
+  })
 }
-const xmlData = [
-  { id: 1, name: '员工编号', prop: 'id' },
-  { id: 2, name: '员工姓名', prop: 'name' },
-  { id: 3, name: '组织', prop: 'organization' },
-  { id: 4, name: '部门', prop: 'department' },
-  { id: 5, name: '职务', prop: 'position' },
-  { id: 6, name: '岗位', prop: 'job' },
-  {
-    id: 7,
-    name: '到本单位时间',
-    prop: 'comeTime'
-  },
-  {
-    id: 8,
-    name: '试用到期时间',
-    prop: 'expirTime'
-  }
-]
-const tableData: User[] = [
-  {
-    id: 10001,
-    name: '彭于晏',
-    organization: '集团总部',
-    department: '企业部门',
-    position: '总规划师',
-    job: '培训管理',
-    comeTime: '2019-03-05',
-    expirTime: '2023-06-01'
-  },
-  {
-    id: 10002,
-    name: '彭于',
-    organization: '集团分部',
-    department: '企业部门',
-    position: '总规划师',
-    job: '培训管理',
-    comeTime: '2019-03-05',
-    expirTime: '2022-06-01'
-  },
-  {
-    id: 10003,
-    name: '龚宇',
-    organization: '集团总部',
-    department: '企业部门',
-    position: '规划师',
-    job: '培训管理',
-    comeTime: '2012-03-05',
-    expirTime: '2013-06-01'
-  },
-  {
-    id: 10004,
-    name: '彭的晏',
-    organization: '集团总部',
-    department: '企业部门',
-    position: '总规划师',
-    job: '培训管理',
-    comeTime: '2019-03-05',
-    expirTime: '2023-06-01'
-  },
-  {
-    id: 10005,
-    name: '彭于让',
-    organization: '集团总部',
-    department: '企业部门',
-    position: '总规划师',
-    job: '培训管理',
-    comeTime: '2019-03-05',
-    expirTime: '2023-06-01'
-  },
-  {
-    id: 10006,
-    name: '彭发的',
-    organization: '集团总部',
-    department: '企业部门',
-    position: '总规划师',
-    job: '培训管理',
-    comeTime: '2019-03-05',
-    expirTime: '2023-06-01'
-  }
-]
-//test
-const pagesizes = ref([1, 2, 3, 4, 5])
-const pagenumber = ref(5)
+const newXmlData = ref([])
+newXmlData.value = [...$store.xmlData]
 </script>
 
-<style></style>
+<style>
+.footer {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
