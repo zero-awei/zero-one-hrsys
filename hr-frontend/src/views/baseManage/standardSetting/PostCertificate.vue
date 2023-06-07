625a9b6df3b0ef031f0aa4a7c1fcaad93ba959ce
微微 <15272516991@163.com>
2023-06-07 19:54:15 +0800

    合并gdmx分支

--- hr-frontend/src/views/baseManage/standardSetting/PostCertificate.vue
+++ hr-frontend/src/views/baseManage/standardSetting/PostCertificate.vue
@@ -1,20 +1,20 @@
 <template>
   <div>
     <div class="header">
-      <TableHead :tableTitle="$store.tableTitle" 
-                :tableOperations ="$store.tableOperations" 
-                :saveData="saveData" 
-                :addTitle= "$store.addTitle" 
-                :dataitem="$store.dataitem"
-                :deleteData="deleteData"
+      <TableHead
+        :tableTitle="$store.tableTitle"
+        :tableOperations="$store.tableOperations"
+        :saveData="saveData"
+        :addTitle="$store.addTitle"
+        :dataitem="$store.dataitem"
       />
     </div>
     <div class="table">
-      <MainTable :xmlData="newXmlData" :tableData="$store.tableData"/>
+      <MainTable :xmlData="newXmlData" :tableData="$store.tableData" />
     </div>
     <div class="footer">
-      <ColumnFilter :xmlData="$store.xmlData" :parentMethod="getNewXmlData"/>
-      <Pagination :pageSizes="$store.pageSizes" :total="$store.total"/>
+      <ColumnFilter :xmlData="$store.xmlData" :parentMethod="getNewXmlData" />
+      <Pagination :pageSizes="$store.pageSizes" :total="$store.total" />
     </div>
   </div>
 </template>
@@ -24,22 +24,14 @@ import TableHead from '@/components/table/head/TableHead.vue'
 import MainTable from '@/components/MainTable.vue'
 import ColumnFilter from '@/components/columnFilter/ColumnFilter.vue'
 import Pagination from '@/components/pagination/Pagination.vue'
-import {usePostCertificateStore} from '@/stores/postCertificate'
-import { onBeforeMount } from 'vue'
+import { usePostCertificateStore } from '@/stores/postCertificate'
 
 const $store = usePostCertificateStore()
-//页面挂载前请求数据
-onBeforeMount(()=>{
-  $store.loadTableData()
-})
+$store.initTableData()
 
 //将新增的数据保存
-const saveData = (val)=>{
+const saveData = (val) => {
   $store.addData(val)
-}
-//书写数据删除逻辑
-const deleteData = (val)=>{
-  console.log(val)
 }
 
 function getNewXmlData(checkStatus) {
@@ -49,11 +41,10 @@ function getNewXmlData(checkStatus) {
 }
 const newXmlData = ref([])
 newXmlData.value = [...$store.xmlData]
-
 </script>
 
 <style lang="scss" scoped>
-.footer{
+.footer {
   display: flex;
   justify-content: space-between;
   align-items: center;
